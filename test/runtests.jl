c666bb730b645eef80bf6f85d70a7ca9d83f9589
Sebastian Pokutta <23001135+pokutta@users.noreply.github.com>
2021-01-14 00:33:23 +0100

    added verbose also for other tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -73,9 +73,9 @@ end
         grad(x) = 2x;
         lmo_prob = FrankWolfe.ProbabilitySimplexOracle(1)
         x0 = FrankWolfe.compute_extreme_point(lmo_prob, zeros(5))
-        @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.agnostic)[3] - 0.2) < 1.0e-5
-        @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.goldenratio)[3] - 0.2) < 1.0e-5
-        @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.backtracking)[3] - 0.2) < 1.0e-5
+        @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.agnostic,verbose=true)[3] - 0.2) < 1.0e-5
+        @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.goldenratio,verbose=true)[3] - 0.2) < 1.0e-5
+        @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.backtracking,verbose=true)[3] - 0.2) < 1.0e-5
     end
     @testset "Testing emphasis blas vs memory" begin
         n = Int(1e5);
