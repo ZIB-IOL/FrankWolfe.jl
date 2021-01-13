c415ccc92ef548bba558d2cb836dc8ba68257769
Sebastian Pokutta <23001135+pokutta@users.noreply.github.com>
2021-01-14 00:31:01 +0100

    - added simple benchmarking for oracles- basic reporting- added emphasis for memory vs. blas- adding simple benchmarking script in tests- added test for emphasis

--- src/FrankWolfe.jl
+++ src/FrankWolfe.jl
@@ -1,6 +1,11 @@
 module FrankWolfe
 
 using LinearAlgebra
+using Printf
+using Base.Ryu
+using ProgressMeter
+# using BenchmarkTools
+using TimerOutputs
 
 include("defs.jl")
 include("simplex_matrix.jl")
@@ -8,15 +13,73 @@ include("oracles.jl")
 include("simplex_oracle.jl")
 include("utils.jl")
 
+
+# simple beyond of core costs of algorithm
+
+function benchmarkOracles(f,grad,lmo,n;k=100,T=Float64)
+    sv = n*sizeof(T)/1024/1024
+    println("\nSize of single vector ($T): $sv MB\n")
+    to = TimerOutput()
+    @showprogress 1 "Testing f..." for i in 1:k    
+        x = rand(n)
+        @timeit to "f" temp = f(x)
+    end
+    @showprogress 1 "Testing grad..." for i in 1:k    
+        x = rand(n)
+        @timeit to "grad" temp = grad(x)
+    end
+    @showprogress 1 "Testing lmo..." for i in 1:k    
+        x = rand(n)
+        @timeit to "lmo" temp = compute_extreme_point(lmo, x)
+    end
+    @showprogress 1 "Testing dual gap..." for i in 1:k    
+        @timeit to "dual gap" begin
+            x = rand(n)
+            gradient = grad(x)
+            v = compute_extreme_point(lmo, gradient)
+            dualGap = dot(x, gradient) - dot(v, gradient)
+        end
+    end
+    @showprogress 1 "Testing update... (emph: blas)" for i in 1:k    
+        x = rand(n)
+        gradient = grad(x)
+        v = compute_extreme_point(lmo, gradient)
+        gamma = 1/2
+        @timeit to "update (blas)" x = (1-gamma) * x + gamma * v
+    end
+    @showprogress 1 "Testing update... (emph: memory)" for i in 1:k    
+        x = rand(n)
+        gradient = grad(x)
+        v = compute_extreme_point(lmo, gradient)
+        gamma = 1/2
+        # TODO: to be updated to broadcast version once data structure MaybeHotVector allows for it
+        @timeit to "update (memory)" x = (1-gamma) * x + gamma * v
+    end
+    print_timer(to::TimerOutput)
+end
+
 # very simple FW Variant
 # TODO:
-# - need to remove unnecessary outputs (uses currently tabulate via pycall)
-# - should support "silent mode" with no printing
 # - should support "performance mode" with minimal computation and printing of intermediate information
 
 
 function fw(f, grad, lmo, x0; stepSize::LSMethod = agnostic, 
-        epsilon=1e-7, maxIt=10000, printIt=1000, trajectory=false, verbose=false,lsTol=1e-7) where T
+        epsilon=1e-7, maxIt=10000, printIt=1000, trajectory=false, verbose=false,lsTol=1e-7,emph::Emph = blas) where T
+    
+    function headerPrint(data)
+        @printf("\n───────────────────────────────────────────────────────────────────────────────────\n")
+        @printf("%6s %13s %14s %14s %14s %14s\n", data[1], data[2], data[3], data[4], data[5], data[6])
+        @printf("───────────────────────────────────────────────────────────────────────────────────\n")
+    end
+
+    function footerPrint()
+        @printf("───────────────────────────────────────────────────────────────────────────────────\n\n")
+    end
+    
+    function itPrint(data)
+        @printf("%6s %13s %14e %14e %14e %14e\n", data[1], data[2], data[3], data[4], data[5], data[6])
+    end
+
     t = 0
     dualGap = Inf
     primal = Inf
@@ -25,13 +88,17 @@ function fw(f, grad, lmo, x0; stepSize::LSMethod = agnostic,
     trajData = []
     dx = similar(x0) # Array{eltype(x0)}(undef, length(x0))
     timeEl = time_ns()
-    # for table printing // to be replaced
-    #
-    # headers = ["Type", "Iteration", "Primal", "Dual", "Dual Gap","Time"]
-    # width = [6, 13, 14, 14, 14, 14]
-    # if verbose
-    #     println(tp.header(headers, width=width))
-    # end
+
+    if verbose
+        if emph === blas
+            println("\n EMPHASIS: blas")
+        elseif emph === memory
+            println("\n EMPHASIS: memory")
+        end
+        headers = ["Type", "Iteration", "Primal", "Dual", "Dual Gap","Time"]
+        headerPrint(headers)
+    end
+    
     while t <= maxIt && dualGap >= max(epsilon,eps())
         primal = f(x)
         gradient = grad(x)
@@ -50,23 +117,27 @@ function fw(f, grad, lmo, x0; stepSize::LSMethod = agnostic,
            nothing, gamma = backtrackingLS(f,grad,x,v,lsTol=lsTol) 
         end
         
-        # that's the expensive version in terms of memory but fast
-        x = (1-gamma) * x + gamma * v
-        
+        if emph === blas
+            x = (1-gamma) * x + gamma * v
+        elseif emph === memory
+            @. x = (1-gamma) * x + gamma * v 
+        end
+
         if mod(t,printIt) == 0 && verbose
             tt = "FW"
             if t === 0
                 tt = "I"
             end
             rep = [tt, string(t), primal, primal-dualGap, dualGap, (time_ns() - timeEl)/1.0e9]
-            # timeEl = time_ns()
-            # println(tp.row(rep,width=width,format_spec="5e"))
+            itPrint(rep)
             flush(stdout)
         end
         t = t + 1
     end
     if verbose
-        # println(tp.bottom(length(width),width=width))
+        tt = "Last"
+        rep = [tt, string(t), primal, primal-dualGap, dualGap, (time_ns() - timeEl)/1.0e9]
+        footerPrint()
         flush(stdout)
     end
     return x, v, primal, dualGap, trajData
--- test/runtests.jl
+++ test/runtests.jl
@@ -77,4 +77,27 @@ end
         @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.goldenratio)[3] - 0.2) < 1.0e-5
         @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.backtracking)[3] - 0.2) < 1.0e-5
     end
+    @testset "Testing emphasis blas vs memory" begin
+        n = Int(1e5);
+        k = 100
+        xpi = rand(n);
+        total = sum(xpi);
+        xp = xpi ./ total;        
+        f(x) = LinearAlgebra.norm(x-xp)^2
+        grad(x) = 2 * (x-xp)
+
+        lmo_prob = FrankWolfe.ProbabilitySimplexOracle(1);
+        x0 = FrankWolfe.compute_extreme_point(lmo_prob, zeros(n));
+
+        x, v, primal, dualGap, trajectory = FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=k,
+            stepSize=FrankWolfe.backtracking,printIt=k/10,verbose=true,emph=FrankWolfe.blas);
+        
+        @test x != nothing 
+
+        x, v, primal, dualGap, trajectory = FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=k,
+            stepSize=FrankWolfe.backtracking,printIt=k/10,verbose=true,emph=FrankWolfe.memory);
+        
+        @test x != nothing
+
+    end
 end
