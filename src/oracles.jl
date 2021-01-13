e3b4b4694f9e8a76202424b3473aadc4f12fe7f8
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-01-13 18:40:52 +0100

    minor doc and CI change

--- src/oracles.jl
+++ src/oracles.jl
@@ -2,7 +2,7 @@
 """
 Supertype for linear minimization oracles.
 
-All LMOs must implement `compute_extreme_point(direction)`
+All LMOs must implement `compute_extreme_point(lmo::LMO, direction)`
 and return a vector `v` of the appropriate type.
 """
 abstract type LinearMinimizationOracle
