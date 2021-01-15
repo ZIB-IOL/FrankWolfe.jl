e6ae633da67d1c647e5a163551c837c9a4a65559
Sebastian Pokutta <23001135+pokutta@users.noreply.github.com>
2021-01-15 14:41:34 +0100

    intermediate checkin for lazy abstraction

--- src/defs.jl
+++ src/defs.jl
@@ -1,2 +1,4 @@
 @enum LSMethod agnostic=1 backtracking=2 goldenratio=3 nonconvex=4 shortstep=5 fixed=6
 @enum Emph blas=1 memory=2
+@enum StepType FW=1 lazy=1 lazylazy=2
+
--- src/oracles.jl
+++ src/oracles.jl
@@ -15,3 +15,35 @@ Computes the point `argmin_{v ∈ C} v ⋅ direction`
 with `C` the set represented by the LMO.
 """
 function compute_extreme_point end
+
+
+
+"""
+lazified lmos
+# TODO
+
+"""
+
+function lazy_compute_extreme_point_threshold(lmo::LinearMinimizationOracle, direction, threshold)
+    if !isempty(lmo.cache)
+        tt::StepType = lazylazy ## optimistically lazy -> reused last point
+        v = lmo.v
+        if dot(v, direction) > threshold # be optimistic: true last returned point first
+            tt = lazy ## just lazy -> used point from cache
+            test = (x -> dot(x, direction)).(lmo.cache) 
+            v = lmo.cache[argmin(test)]
+        end
+        if dot(v, direction) > threshold  # still not good enough, then solve the LP
+            tt = fw ## no cache point -> used the expensive LP
+            v = compute_extreme_point(lmo, direction)
+            if !(v in lmo.cache) 
+                push!(lmo.cache,v)
+            end
+        end
+    else    
+        v = compute_extreme_point(lmo, direction)
+        push!(lmo.cache,v)
+    end
+    lmo.v = v
+    return v, dot(v,direction), tt
+end
