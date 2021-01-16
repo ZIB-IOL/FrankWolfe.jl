5acae544ce5e926389ac53e63b7695a4a7acdbde
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-01-16 17:49:18 +0100

    momentum first try

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,7 +16,7 @@ end
 @testset "FrankWolfe.jl" begin
     @testset "Testing vanilla Frank-Wolfe with various step size strategies" begin
         f(x) = LinearAlgebra.norm(x)^2
-        grad(x) = 2x;
+        grad(x) = 2x
         lmo_prob = FrankWolfe.ProbabilitySimplexOracle(1)
         x0 = FrankWolfe.compute_extreme_point(lmo_prob, zeros(5))
         @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.agnostic,verbose=true)[3] - 0.2) < 1.0e-5
@@ -25,6 +25,8 @@ end
         @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.nonconvex,verbose=true)[3] - 0.2) < 1.0e-2
         @test FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.shortstep,L=2,verbose=true)[3] ≈ 0.2 
         @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.nonconvex,verbose=true)[3] - 0.2) < 1.0e-2
+        # x0 = 1.0 * FrankWolfe.compute_extreme_point(lmo_prob, zeros(5))
+        # @test abs(FrankWolfe.fw(f,grad,lmo_prob,x0,maxIt=1000,stepSize=FrankWolfe.agnostic,verbose=false, momentum = 0.9)[3] - 0.2) < 1.0e-3
     end
     @testset "Testing Lazified Conditional Gradients with various step size strategies" begin
         f(x) = LinearAlgebra.norm(x)^2
