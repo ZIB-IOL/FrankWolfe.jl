c2c655ca01c280deeec1812e20dad48945c738e1
Sebastian Pokutta <23001135+pokutta@users.noreply.github.com>
2021-01-16 12:05:37 +0100

    added a large-scale and an approx caratheodory example

---
+++ examples/approximateCaratheodory.jl
@@ -0,0 +1,42 @@
+
+using FrankWolfe
+using LinearAlgebra
+
+n = Int(1e2);
+k = n 
+
+const f(x) = dot(x,x)
+const grad(x) = 2 * (x) 
+
+# pick feasible region
+lmo = FrankWolfe.ProbabilitySimplexOracle{Rational{BigInt}}(1); # radius needs to be integer or rational
+
+# compute some initial vertex
+x0 = FrankWolfe.compute_extreme_point(lmo, zeros(n));
+
+# verify that the output is really rational
+println("Output type of LMO: ", eltype(x0))
+
+# benchmarking Oracles
+FrankWolfe.benchmarkOracles(f,grad,lmo,n;k=100,T=Float64)
+
+# the algorithm runs in rational arithmetic even if the gradients and the function itself are not rational
+# this is because we replace the descent direction by the directions of the LMO are rational
+
+@time x, v, primal, dualGap, trajectory = FrankWolfe.fw(f,grad,lmo,x0,maxIt=k,
+    stepSize=FrankWolfe.agnostic,printIt=k/10,verbose=true);
+
+println("\nOutput type of solution: ", eltype(x))
+
+# you can even run everything in rational arithmetic using the shortstep rule
+# NOTE: in this case the gradient computation has to be rational as well
+
+@time x, v, primal, dualGap, trajectory = FrankWolfe.fw(f,grad,lmo,x0,maxIt=k,
+    stepSize=FrankWolfe.rationalshortstep,L=2,printIt=k/10,verbose=true);
+
+println("\nOutput type of solution: ", eltype(x))
+
+println("\nNote: the last step where we exactly close the gap. This is not an error. ")
+fract = 1//n
+println("We have *exactly* computed the optimal solution with with the $fract * (1, ..., 1) vector.\n")
+println("x = $x")
---
+++ examples/largeScale.jl
@@ -0,0 +1,30 @@
+import FrankWolfe
+import LinearAlgebra
+
+
+n = Int(1e9)
+k = 10000
+
+xpi = rand(n);
+total = sum(xpi);
+const xp = xpi ./ total;
+
+const f(x) = LinearAlgebra.norm(x-xp)^2
+const grad(x) = 2 * (x-xp)
+
+
+function cf(x,xp)
+    return @. LinearAlgebra.norm(x-xp)^2
+end
+
+function cgrad(x,xp)
+    return @. 2 * (x-xp)
+end
+
+lmo = FrankWolfe.ProbabilitySimplexOracle(1);
+x0 = FrankWolfe.compute_extreme_point(lmo, zeros(n));
+
+FrankWolfe.benchmarkOracles(x -> cf(x,xp),x -> cgrad(x,xp),lmo,n;k=100,T=Float64)
+
+@time x, v, primal, dualGap, trajectory = FrankWolfe.fw(f,grad,lmo,x0,maxIt=k,
+    stepSize=FrankWolfe.agnostic,printIt=k/10,verbose=true);
