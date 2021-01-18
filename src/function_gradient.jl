b5d0cc1a0d18aba0d6c07c6bb54fea7a415390f2
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-01-18 19:20:01 +0100

    SFW and tests on stochastic gradient

--- src/function_gradient.jl
+++ src/function_gradient.jl
@@ -1,4 +1,13 @@
 
+"""
+    ObjectiveFunction{VT}
+
+Represents an objective function optimized by algorithms.
+Subtypes of `ObjectiveFunction` must implement at least
+* `compute_value(::ObjectiveFunction, x)` for primal value evaluation
+* `compute_gradient(::ObjectiveFunction, x)` for gradient evaluation.
+and optionally `compute_value_gradient(::ObjectiveFunction, x)` returning the (primal, gradient) pair.
+"""
 abstract type ObjectiveFunction
 end
 
@@ -6,65 +15,78 @@ function compute_value end
 
 function compute_gradient end
 
-function compute_value_gradient end
+"""
+    compute_value_gradient(f::ObjectiveFunction, x; kwargs)
 
-function gradient_type end
+Computes in one call the pair `(function_value, function_grad)` evaluated at `x`.
+By default, calls `compute_value` and `compute_gradient` with keyword `kwargs`
+passed to both.
+"""
+compute_value_gradient(f::ObjectiveFunction, x; kwargs...) = (compute_value(f, x; kwargs...), compute_gradient(f, x; kwargs...))
 
-struct SimpleFunction{VT, F, G} <: ObjectiveFunction
+struct SimpleFunctionObjective{F, G} <: ObjectiveFunction
     f::F
     grad::G
 end
 
-SimpleFunction(f::F, grad::G) where {F, G} = SimpleFunction{Vector{Float64}, F, G}(f, g)
-
-compute_value(f::SimpleFunction, x) = f.f(x)
-compute_gradient(f::SimpleFunction, x) = f.grad(x)
-compute_value_gradient(f::SimpleFunction, x) = (f.f(x), f.grad(x))
-
-gradient_type(::SimpleFunction{VT}) where {VT} = VT
+compute_value(f::SimpleFunctionObjective, x) = f.f(x)
+compute_gradient(f::SimpleFunctionObjective, x) = f.grad(x)
 
 """
-    StochasticGradientObjective{VT, F, G, XT}(f::F, grad::G, xs::XT)
+    StochasticObjective{F, G, XT}(f::F, grad::G, xs::XT)
 
 Represents an objective function evaluated with stochastic gradient.
 `f(θ, x)` evaluates the loss for data point `x` and parameter `θ`.
 `grad(θ, x)` evaluates the loss gradient with respect to data point `x` at parameter `θ`.
-`xs` must be an indexable iterable (by default a `Vector{Vector{Float64}}`).
-`VT` is the type returned by `grad(θ, x)`.
+`xs` must be an indexable iterable (`Vector{Vector{Float64}}` for instance).
+Functions using a `StochasticObjective` have optional keyword arguments `rng`, `batch_size`
+and `full_evaluation` controlling whether the function should be evaluated over all data points.
 """
-struct StochasticObjective{VT, F, G, XT} <: ObjectiveFunction
+struct StochasticObjective{F, G, XT} <: ObjectiveFunction
     f::F
     grad::G
     xs::XT
 end
 
-function StochasticObjective{VT}(f::F, grad::G, xs::XT) where {VT, F, G, XT}
-    return StochasticObjective{VT, F, G, XT}(f::F, grad::G, xs::XT)
-end
-
-function compute_value(f::StochasticObjective, θ; batch_size::Integer=length(f.xs), rng=Random.GLOBAL_RNG)
-    rand_indices = rand(rng, eachindex(f.xs), batch_size)
+function compute_value(f::StochasticObjective, θ; batch_size::Integer=length(f.xs), rng=Random.GLOBAL_RNG, full_evaluation=false)
+    rand_indices = if full_evaluation
+        eachindex(f.xs)
+    else
+        rand(rng, eachindex(f.xs), batch_size)
+    end
     return sum(f.f(θ, f.xs[idx]) for idx in rand_indices)
 end
 
-function compute_gradient(f::StochasticObjective, θ; batch_size::Integer=length(f.xs) ÷ 10, rng=Random.GLOBAL_RNG)
-    rand_indices = rand(rng, eachindex(f.xs), batch_size)
+function compute_gradient(f::StochasticObjective, θ; batch_size::Integer=length(f.xs) ÷ 10 + 1, rng=Random.GLOBAL_RNG, full_evaluation=false)
+    rand_indices = if full_evaluation
+        eachindex(f.xs)
+    else
+        rand(rng, eachindex(f.xs), batch_size)
+    end
     return sum(f.grad(θ, f.xs[idx]) for idx in rand_indices)
 end
 
-function compute_value_gradient(f::StochasticObjective, θ; batch_size::Integer=length(f.xs) ÷ 10, rng=Random.GLOBAL_RNG)
-    rand_indices = rand(rng, eachindex(f.xs), batch_size)
-    # reduce: take partial value and gradient, and new index, add value and gradient wrt new point
-    function reduce_op(partial_values, idx)
-        (f_val, g_val) = partial_values
-        x = @inbounds f.xs[idx]
-        (f_val + f.f(θ, x), g_val + f.grad(θ, x))
+function compute_value_gradient(f::StochasticObjective, θ; batch_size::Integer=length(f.xs) ÷ 10 + 1, rng=Random.GLOBAL_RNG, full_evaluation=false)
+    rand_indices = if full_evaluation
+        eachindex(f.xs)
+    else
+        rand(rng, eachindex(f.xs), batch_size)
     end
-    mapreduce(
-        idx -> (f.f(θ, f.xs[idx]), f.grad(θ, f.xs[idx])), # map operation
+    # map operation, for each index, computes value and gradient
+    function map_op(idx)
+        @inbounds x = f.xs[idx]
+        return (f.f(θ, x), f.grad(θ, x))
+    end
+    # reduce: take partial value and gradient, adds value and gradient wrt new point
+    function reduce_op(left_tup, right_tup)
+        (f_val, g_val) = left_tup
+        (f_new, g_new) = right_tup
+        (f_val + f_new, g_val + g_new)
+    end
+
+    return mapfoldr(
+        map_op,
         reduce_op,
         rand_indices,
     )
 end
-
-gradient_type(::StochasticObjective{VT}) where {VT} = VT
