2d1c16a5cd97b650dad4fe677f06c20c40715041
Sebastian Pokutta <23001135+pokutta@users.noreply.github.com>
2021-02-14 16:15:28 +0100

    intermediate

--- examples/largeScale.jl
+++ examples/largeScale.jl
@@ -1,7 +1,7 @@
 import FrankWolfe
 import LinearAlgebra
 
-n = Int(1e9)
+n = Int(1e4)
 k = 1000
 
 xpi = rand(n);
@@ -16,10 +16,10 @@ end
 # better for memory consumption as we do coordinate-wise ops
 
 function cf(x, xp)
-    return @. norm(x - xp)^2
+    return @. LinearAlgebra.norm(x - xp)^2
 end
 
-function cgrad(storage, x, xp)
+function cgrad!(storage, x, xp)
     return @. storage = 2 * (x - xp)
 end
 
@@ -33,6 +33,7 @@ FrankWolfe.benchmark_oracles(x -> cf(x, xp), (str, x) -> cgrad!(str, x, xp), lmo
     (str, x) -> cgrad!(str, x, xp),
     lmo,
     x0,
+    nep=false,
     max_iteration=k,
     line_search=FrankWolfe.agnostic,
     print_iter=k / 10,
