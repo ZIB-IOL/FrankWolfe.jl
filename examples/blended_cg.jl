48cb8e1941aa7cd8ec24ae4dd721e8aa137df516
alejandro-carderera <alejandro.carderera@gmail.com>
2021-03-11 17:44:26 +0100

    Delete old code and update test file.
    Add an example over the probability simplex that utilizes MaybeHotVectors, and an example over the K-Sparse LMO that uses sparse arrays.

--- examples/blended_cg.jl
+++ examples/blended_cg.jl
@@ -5,8 +5,8 @@ using DoubleFloats
 using FrankWolfe
 using SparseArrays
 
-n = Int(5e2)
-k = 1000
+n = Int(1000)
+k = 10000
 
 s = rand(1:100)
 @info "Seed $s"
@@ -15,27 +15,7 @@ s = rand(1:100)
 s = 41
 Random.seed!(s)
 
-xpi = rand(n);
-total = sum(xpi);
-const xp = xpi # ./ total;
-
-#matrix = rand(n, n)
-#hessian = matrix * transpose(matrix)
-#linear_term = rand(n)
-#constant_term = rand()
-#f(x) = constant_term + dot(linear_term, x) + 0.5* transpose(x) * hessian * x
-#function grad!(storage, x)
-#    storage .= linear_term +  hessian * x
-#end
-#gradient = zeros(n)
-
-
-#f(x) = norm(x - xp)^2
-#function grad!(storage, x)
-#    @. storage = 2 * (x - xp)
-#end
-#hessian = Matrix(2.0I, n, n) 
-
+"""
 matrix = rand(n,n)
 hessian = transpose(matrix) * matrix
 linear = rand(n)
@@ -45,45 +25,9 @@ function grad!(storage, x)
 end
 L = eigmax(hessian)
 
-# better for memory consumption as we do coordinate-wise ops
-
-function cf(x, xp)
-    return @. norm(x - xp)^2
-end
-
-function cgrad!(storage, x, xp)
-    return @. storage = 2 * (x - xp)
-end
-
-# this LMO might produce numerical instabilities do demonstrate the recovery feature
-const lmo = FrankWolfe.KSparseLMO(50, 1.0)
-
-# full upgrade of the lmo (and hence optimization) to Double64.
-# the same lmo with Double64 is much more numerically robust. costs relatively little in speed.
- #const lmo = FrankWolfe.KSparseLMO(100, Double64(1.0))
-
-# as above but now to bigfloats
-# the same lmo here with bigfloat. even more robust but much slower
-# const lmo = FrankWolfe.KSparseLMO(100, big"1.0")
-
-# other oracles to test / experiment with
-# const lmo = FrankWolfe.LpNormLMO{Float64,1}(1.0)
-#const lmo = FrankWolfe.ProbabilitySimplexOracle(Double64(1.0));
-#const lmo = FrankWolfe.ProbabilitySimplexOracle(1.0);
-# const lmo = FrankWolfe.UnitSimplexOracle(1.0);
-
-const x00 = FrankWolfe.compute_extreme_point(lmo, zeros(n))
-
-#active_set = FrankWolfe.ActiveSet([(1.0, x00)]) 
-#vect = FrankWolfe.compute_extreme_point(lmo, rand(n))
-#FrankWolfe.active_set_update!(active_set, 0.2, vect)
-#vect = FrankWolfe.compute_extreme_point(lmo, rand(n))
-#FrankWolfe.active_set_update!(active_set, 0.2, vect)
-
-FrankWolfe.benchmark_oracles(x -> cf(x, xp), (str, x) -> cgrad!(str, x, xp), ()->randn(n), lmo; k=100)
-
-# copying here and below the x00 as the algorithms write back into the variables to save memory.
-# as we do multiple runs from the same initial point we do not want this here.
+#Run over the probability simplex
+lmo = FrankWolfe.ProbabilitySimplexOracle(1.0);
+x00 = FrankWolfe.compute_extreme_point(lmo, zeros(n))
 
 x0 = deepcopy(x00)
 x, v, primal, dual_gap, trajectoryBCG_accel_simplex = FrankWolfe.bcg(
@@ -140,8 +84,65 @@ x, v, primal, dual_gap, trajectoryBCG_convex = FrankWolfe.bcg(
     weight_purge_threshold=1e-10,
 )
 
+data = [trajectoryBCG_accel_simplex, trajectoryBCG_simplex, trajectoryBCG_convex]
+label = ["BCG (accel simplex)", "BCG (simplex)", "BCG (convex)"]
+FrankWolfe.plot_trajectories(data, label)
+"""
+
+
+matrix = rand(n,n)
+hessian = transpose(matrix) * matrix
+linear = rand(n)
+f(x) = dot(linear, x) + 0.5*transpose(x) * hessian * x
+function grad!(storage, x)
+    storage .= linear + hessian * x
+end
+L = eigmax(hessian)
+
+#Run over the K-sparse polytope
+lmo = FrankWolfe.KSparseLMO(100, 100.0)
+x00 = FrankWolfe.compute_extreme_point(lmo, zeros(n))
+
 x0 = deepcopy(x00)
-x, v, primal, dual_gap, trajectoryBCG_convex_backup = FrankWolfe.bcg_backup(
+x, v, primal, dual_gap, trajectoryBCG_accel_simplex = FrankWolfe.bcg(
+    f,
+    grad!,
+    lmo,
+    x0,
+    max_iteration=k,
+   line_search=FrankWolfe.adaptive,
+    print_iter=k / 10,
+    hessian = hessian,
+    emphasis=FrankWolfe.memory,
+    L=L,
+    accelerated = true,
+    verbose=true,
+    trajectory=true,
+    Ktolerance=1.00,
+    weight_purge_threshold=1e-10,
+)
+
+x0 = deepcopy(x00)
+x, v, primal, dual_gap, trajectoryBCG_simplex = FrankWolfe.bcg(
+    f,
+    grad!,
+    lmo,
+    x0,
+    max_iteration=k,
+    line_search=FrankWolfe.adaptive,
+    print_iter=k / 10,
+    hessian = hessian,
+    emphasis=FrankWolfe.memory,
+    L=L,
+    accelerated = false,
+    verbose=true,
+    trajectory=true,
+    Ktolerance=1.00,
+    weight_purge_threshold=1e-10,
+)
+
+x0 = deepcopy(x00)
+x, v, primal, dual_gap, trajectoryBCG_convex = FrankWolfe.bcg(
     f,
     grad!,
     lmo,
@@ -154,45 +155,9 @@ x, v, primal, dual_gap, trajectoryBCG_convex_backup = FrankWolfe.bcg_backup(
     verbose=true,
     trajectory=true,
     Ktolerance=1.00,
-    goodstep_tolerance=0.95,
     weight_purge_threshold=1e-10,
 )
 
-data = [trajectoryBCG_accel_simplex, trajectoryBCG_simplex, trajectoryBCG_convex, trajectoryBCG_convex_backup]
-label = ["BCG (accel simplex)", "BCG (simplex)", "BCG (convex)", "BCG (convex backup)"]
-FrankWolfe.plot_trajectories(data, label, filename="output_results.png")
-
-#x0 = deepcopy(x00)
-#@time x, v, primal, dual_gap, trajectoryAda = FrankWolfe.afw(
-#    f,
-#    grad!,
-#    lmo,
-#    x0,
-#    max_iteration=k,
-#    line_search=FrankWolfe.adaptive,
-#    L=100,
-#    print_iter=k / 10,
-#    emphasis=FrankWolfe.memory,
-#    verbose=true,
-#    trajectory=true,
-#);
-
-#x0 = deepcopy(x00)
-#@time x, v, primal, dual_gap, trajectorySs = FrankWolfe.fw(
-#    f,
-#    grad!,
-#    lmo,
-#    x0,
-#    max_iteration=k,
-#    line_search=FrankWolfe.shortstep,
-#    L=2,
-#    print_iter=k / 10,
-#    emphasis=FrankWolfe.memory,
-#    verbose=true,
-#    trajectory=true,
-#);
-
-#data = [trajectorySs, trajectoryAda, trajectoryBCG]
-#label = ["short step", "AFW", "BCG"]
-#
-#FrankWolfe.plot_trajectories(data, label)
+data = [trajectoryBCG_accel_simplex, trajectoryBCG_simplex, trajectoryBCG_convex]
+label = ["BCG (accel simplex)", "BCG (simplex)", "BCG (convex)"]
+FrankWolfe.plot_trajectories(data, label)
