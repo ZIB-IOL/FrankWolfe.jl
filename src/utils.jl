026c36c7cf97932dd33a7290c56172813e77a02d
alejandro-carderera <alejandro.carderera@gmail.com>
2021-03-12 17:32:46 +0100

    Make the tolerance an optional parameter with a default value.

--- src/utils.jl
+++ src/utils.jl
@@ -614,11 +614,11 @@ end
 """
 Check if the gradient using finite differences matches the grad! provided.
 """
-function check_gradients(grad!, f, gradient, num_tests = 10)
+function check_gradients(grad!, f, gradient, num_tests = 10, tolerance = 1.0e-5)
     for i in 1:num_tests
         random_point = rand(length(gradient))
         grad!(gradient, random_point)
-        if norm(grad(central_fdm(5, 1), f, random_point)[1] - gradient) > 1.0e-5
+        if norm(grad(central_fdm(5, 1), f, random_point)[1] - gradient) > tolerance
             @warn "There is a noticeable difference between the gradient provided and
             the gradient computed using finite differences."
         end
