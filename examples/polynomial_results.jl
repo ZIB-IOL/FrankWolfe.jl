1a4f847d7dd1f2f75faeb13db3f952bc8e4af036
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-04-09 17:29:25 +0200

    polynomial results in separate file

---
+++ examples/polynomial_results.jl
@@ -0,0 +1,36 @@
+
+include("activate.jl")
+
+using JSON
+using LaTeXStrings
+results = JSON.Parser.parsefile(joinpath(@__DIR__, "polynomial_result.json"))
+
+iteration_list = [[x[1] + 1 for x in results["trajectory_arr_fw"]], [x[1] + 1 for x in results["trajectory_arr_bcg"]], collect(eachindex(results["function_values_gd"]))]
+time_list = [[x[5] for x in results["trajectory_arr_fw"]],[x[5] for x in results["trajectory_arr_bcg"]], results["gd_times"]]
+primal_list = [[x[2] for x in results["trajectory_arr_fw"]], [x[2] for x in results["trajectory_arr_bcg"]], results["function_values_gd"]]
+test_list = [[x[6] for x in results["trajectory_arr_fw"]],[x[6] for x in results["trajectory_arr_bcg"]], results["function_values_test_gd"]]
+label = [L"\textrm{FW}", L"\textrm{BCG}", L"\textrm{GD}"]
+coefficient_error_values = [[x[7] for x in results["trajectory_arr_fw"]], [x[7] for x in results["trajectory_arr_bcg"]], results["coefficient_error_gd"]]
+
+
+FrankWolfe.plot_results(
+    [primal_list, primal_list, test_list, test_list],
+    [iteration_list, time_list, iteration_list, time_list],
+    label,
+    [L"\textrm{Iteration}", L"\textrm{Time}", L"\textrm{Iteration}", L"\textrm{Time}"],
+    [L"\textrm{Training loss}", L"\textrm{Training loss}", L"\textrm{Test loss}", L"\textrm{Test loss}"],
+    xscalelog = [:log, :identity, :log, :identity],
+    legend_position = [:bottomleft, nothing, nothing, nothing],
+    filename="polynomial_result.pdf",
+)
+
+FrankWolfe.plot_results(
+    [coefficient_error_values, coefficient_error_values],
+    [iteration_list, time_list],
+    label,
+    [L"\textrm{Iteration}", L"\textrm{Time}"],
+    [L"\textrm{Coefficient error}", L"\textrm{Coefficient error}"],
+    xscalelog = [:log, :identity],
+    legend_position = [:bottomleft, nothing],
+    filename="polynomial_coefficients.pdf",
+)
--- examples/polynomials.jl
+++ examples/polynomials.jl
@@ -8,6 +8,7 @@ using DynamicPolynomials
 
 import ReverseDiff
 using FiniteDifferences
+import JSON
 
 const N = 9
 
@@ -20,7 +21,7 @@ const var_monomials = MultivariatePolynomials.monomials(X, 0:max_degree)
 Random.seed!(42)
 const all_coeffs = map(var_monomials) do m
     d = MultivariatePolynomials.degree(m)
-    return 10 * rand() .* (rand() .> 0.6 * d / max_degree)
+    return 10 * rand() .* (rand() .> 0.7 * d / max_degree)
 end
 
 const true_poly = dot(all_coeffs, var_monomials)
@@ -32,7 +33,7 @@ function evaluate_poly(coefficients)
     end
 end
 
-const training_data = map(1:1000) do _
+const training_data = map(1:500) do _
     x = 3 * randn(N)
     y = MultivariatePolynomials.subs(true_poly, Pair(X, x)) + 2 * randn()
     return (x, y.a[1])
@@ -43,7 +44,7 @@ const extended_training_data = map(training_data) do (x, y)
     return (x_ext, y)
 end
 
-const test_data = map(1:1000) do _
+const test_data = map(1:5000) do _
     x = 3 * randn(N)
     y = MultivariatePolynomials.subs(true_poly, Pair(X, x)) + 2 * randn()
     return (x, y.a[1])
@@ -66,9 +67,9 @@ function f_test(coefficients)
     end
 end
 
-function matching_zeros(coeffs)
-    return count(eachindex(all_coeffs)) do idx
-        return all_coeffs[idx] ≈ 0 && coeffs[idx] ≈ 0
+function coefficient_errors(coeffs)
+    return 0.5 * sum(eachindex(all_coeffs)) do idx
+        (all_coeffs[idx] - coeffs[idx])^2
     end
 end
 
@@ -82,85 +83,102 @@ function grad!(storage, coefficients)
     return nothing
 end
 
+function build_callback(trajectory_arr)
+    function callback(state)
+        push!(
+            trajectory_arr,
+            (Tuple(state)[1:5]..., f_test(state.x), coefficient_errors(state.x))
+        )
+    end
+end
+
 #Check the gradient using finite differences just in case
 gradient = similar(all_coeffs)
 FrankWolfe.check_gradients(grad!, f, gradient)
 
+max_iter = 10_000
+
 # gradient descent
+
 xgd = rand(length(all_coeffs))
-for iter in 1:10_000
+training_gd = Float64[]
+test_gd = Float64[]
+coeff_error = Float64[]
+time_start = time_ns()
+gd_times = Float64[]
+for iter in 1:max_iter
     global xgd
     grad!(gradient, xgd)
     @. xgd -= 0.00001 * gradient
+    push!(training_gd, f(xgd))
+    push!(test_gd, f_test(xgd))
+    push!(coeff_error, coefficient_errors(xgd))
+    push!(gd_times, (time_ns() - time_start) * 1e-9)
 end
 
 @info "Gradient descent training loss $(f(xgd))"
 @info "Gradient descent test loss $(f_test(xgd))"
-@info "Matching zeros $(matching_zeros(xgd))"
+@info "Coefficient error $(coefficient_errors(xgd))"
 
-lmo = FrankWolfe.KSparseLMO(length(all_coeffs) ÷ 4, 1.1 * maximum(all_coeffs))
+lmo = FrankWolfe.KSparseLMO(2 * length(all_coeffs) ÷ 3, 1.1 * maximum(all_coeffs))
 
 x00 = FrankWolfe.compute_extreme_point(lmo, rand(length(all_coeffs)))
 
-k = 10_000
-
-x0 = deepcopy(x00)
+x0 = collect(x00)
 
 # vanilla FW
-@time x, v, primal, dual_gap, trajectoryFw = FrankWolfe.frank_wolfe(
+trajectory_fw = []
+callback = build_callback(trajectory_fw)
+@time x_fw, v, primal, dual_gap, _ = FrankWolfe.frank_wolfe(
     f,
     grad!,
     lmo,
     x0,
-    max_iteration=k,
+    max_iteration=max_iter,
     line_search=FrankWolfe.adaptive,
-    print_iter=k / 10,
+    print_iter=max_iter ÷ 10,
     emphasis=FrankWolfe.memory,
     verbose=true,
-    trajectory=false,
     gradient=gradient,
+    callback=callback,
 );
 
-@info "Vanilla training loss $(f(x))"
-@info "Test loss $(f_test(x))"
-@info "Matching zeros $(matching_zeros(x))"
+@info "Vanilla training loss $(f(x_fw))"
+@info "Test loss $(f_test(x_fw))"
+@info "Coefficient error $(coefficient_errors(x_fw))"
+
+trajectory_bcg = []
+callback = build_callback(trajectory_bcg)
 
 x0 = deepcopy(x00)
-
-@time x, v, primal, dual_gap, trajectoryFw = FrankWolfe.away_frank_wolfe(
+@time x_bcg, v, primal, dual_gap, _ = FrankWolfe.blended_conditional_gradient(
     f,
     grad!,
     lmo,
     x0,
-    max_iteration=k,
+    max_iteration=max_iter,
     line_search=FrankWolfe.adaptive,
-    print_iter=k / 10,
+    print_iter=max_iter ÷ 10,
     emphasis=FrankWolfe.memory,
     verbose=true,
-    lazy=true,
-    trajectory=false,
-    gradient=gradient,
-);
-
-@info "AFW training loss $(f(x))"
-@info "Test loss $(f_test(x))"
-@info "Matching zeros $(matching_zeros(x))"
-
-x0 = deepcopy(x00)
-@time x, v, primal, dual_gap, trajectoryBCG = FrankWolfe.blended_conditional_gradient(
-    f,
-    grad!,
-    lmo,
-    x0,
-    max_iteration=k,
-    line_search=FrankWolfe.adaptive,
-    print_iter=k / 10,
-    emphasis=FrankWolfe.memory,
-    verbose=true,
-    trajectory=false,
     weight_purge_threshold=1e-10,
+    callback=callback,
 )
 
-@info "BCG training loss $(f(x))"
-@info "Test loss $(f_test(x))"
-@info "Matching zeros $(matching_zeros(x))"
+@info "BCG training loss $(f(x_bcg))"
+@info "Test loss $(f_test(x_bcg))"
+@info "Coefficient error $(coefficient_errors(x_bcg))"
+
+open(joinpath(@__DIR__, "polynomial_result.json"), "w") do f
+    data = JSON.json(
+        (
+            trajectory_arr_fw=trajectory_fw,
+            trajectory_arr_bcg=trajectory_bcg,
+            function_values_gd=training_gd,
+            function_values_test_gd=test_gd,
+            coefficient_error_gd=coeff_error,
+            gd_times=gd_times,
+        )
+    )
+    write(f, data)
+end
