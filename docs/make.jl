265537223df0acd64f38a15a9629ee13df6366ac
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-06-21 12:29:18 +0200

    Update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,4 +7,10 @@ makedocs(
     sitename="FrankWolfe.jl",
     format=Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
     pages=["Home"=>"index.md","Examples"=>"examples2.md","References"=>"reference.md",
-            "Index"=>"indexlist.md"])
+            "Index"=>"indexlist.md"],
+)
+
+deploydocs(
+    repo   = "github.com/ZIB-IOL/FrankWolfe.jl.git",
+    push_preview = true,
+)
