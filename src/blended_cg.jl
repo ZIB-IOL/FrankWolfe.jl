a8330b0856945a6061966a14206bf3c5221ae966
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-06-21 12:52:59 +0200

    Update src/blended_cg.jl

--- src/blended_cg.jl
+++ src/blended_cg.jl
@@ -46,7 +46,7 @@ end
 
 Entry point for the Blended Conditional Gradient algorithm.
 See Braun, Gábor, et al. "Blended conditonal gradients" ICML 2019.
-The method works on an active set like [`away_frank_wolfe`](@ref),
+The method works on an active set like [`FrankWolfe.away_frank_wolfe`](@ref),
 performing gradient descent over the convex hull of active vertices,
 removing vertices when their weight drops to 0 and adding new vertices
 by calling the linear oracle in a lazy fashion.
