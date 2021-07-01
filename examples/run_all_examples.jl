26e9abe057654a3bf3880576ee674dbc44dfb016
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-07-01 11:19:51 +0200

    only 2 random examples

--- examples/run_all_examples.jl
+++ examples/run_all_examples.jl
@@ -1,9 +1,12 @@
+using Random
 
 example_files = filter(readdir(@__DIR__)) do f
     occursin(".jl", f) && !occursin("large", f) && !occursin("result", f) && f != "activate.jl"
 end
 
-for file in example_files
-    @info "running $file"
+example_shuffle = randperm(length(example_files))
+
+for file in example_files[example_shuffle[1:2]]
+    @info "running example $file"
     run(`julia $file`)
 end
