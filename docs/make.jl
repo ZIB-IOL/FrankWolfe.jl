12556c8a6f1b2a92112f1ae00aea045772f9086b
j-geuter <85136388+j-geuter@users.noreply.github.com>
2021-08-19 14:21:27 +0200

    Third example (#224)
    * basic documentation structure
    
    * docs/build in gitignore now
    
    * docs/build/ in .gitignore now
    
    * docs/build/ added to .gitignore
    
    * dollarsigns replaced
    
    * Update ci.yml
    
    * Project.toml update
    
    * docs/Project.toml added
    
    * third example
    
    * third example
    
    * minor fixes
    
    * 4th example included and ZipFile added to Project.toml
    
    * small change
    
    * Update docs/src/examples.md
    
    Co-authored-by: Mathieu Besançon <mathieu.besancon@gmail.com>
    
    * minor fix
    
    * Update docs/src/examples.md
    
    * Update docs/src/examples.md
    
    * update Project.toml
    
    * update plot size
    
    * add Arpack
    
    * Update docs/src/examples.md
    
    * Update docs/src/examples.md
    
    * New Examples Structure
    
    * New Examples Structure
    
    * Delete examples.md
    
    Co-authored-by: Mathieu Besançon <mathieu.besancon@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,12 @@ makedocs(
     format=Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true"),
     pages=[
         "Home" => "index.md",
-        "Examples" => "examples.md",
+        "Examples" => [
+            "Comparison with MathOptInterface on a Probability Simplex" => "example1.md",
+            "Polynomial Regression" => "example2.md",
+            "Matrix Completion" => "example3.md",
+            "Exact Optimization with Rational Arithmetic" => "example4.md",
+        ],
         "References" => "reference.md",
         "Index" => "indexlist.md",
     ],
