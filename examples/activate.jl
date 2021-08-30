8cf49dd35616bbdfc49916aa35b39be01385daa7
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-08-30 16:43:27 +0200

    make sure env is activated

--- examples/activate.jl
+++ examples/activate.jl
@@ -1,7 +1,11 @@
-using TestEnv
 import Pkg
+Pkg.activate(@__DIR__)
+
+using TestEnv
+
 Pkg.activate(joinpath(@__DIR__, ".."))
 TestEnv.activate()
+
 using FrankWolfe
 using ProgressMeter
 using Arpack
