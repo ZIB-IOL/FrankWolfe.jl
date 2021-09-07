1b48dd41573198a9cf5da8129834f43714b6f7a6
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-09-07 17:02:29 +0200

    rename example files, complete rational one

--- docs/src/examples/4_rational_opt.jl
+++ docs/src/examples/4_rational_opt.jl
@@ -6,25 +6,61 @@
 # package, this simply means initializing the radius with a rational-compatible element type, e.g., `1`, rather
 # than a floating-point number, e.g., `1.0`. Given that numerators and denominators can become quite large in
 # rational arithmetic, it is strongly advised to base the used rationals on extended-precision integer types such
-# as `BigInt`, i.e., we use `Rational{BigInt}`. For the probability simplex LMO with a rational radius of `1`,
-# the LMO would be created as follows:
+# as `BigInt`, i.e., we use `Rational{BigInt}`.
 
-# ```
-# lmo = FrankWolfe.ProbabilitySimplexOracle{Rational{BigInt}}(1)
-# ```
-
-# As mentioned before, the second requirement ensuring that the computation runs in rational arithmetic is
+# The second requirement ensuring that the computation runs in rational arithmetic is
 # a rational-compatible step-size rule. The most basic step-size rule compatible with rational optimization is
 # the agnostic step-size rule with ``\gamma_t = 2/(2 + t)``. With this step-size rule, the gradient does not even need to
 # be rational as long as the atom computed by the LMO is of a rational type. Assuming these requirements are
-# met, all iterates and the computed solution will then be rational:
+# met, all iterates and the computed solution will then be rational.
 
+using LinearAlgebra
+k = n
 
 n = 100
 x = fill(big(1)//100, n)
-## equivalent to { 1/100 }^100
 
+f(x) = dot(x, x)
+function grad!(storage, x)
+    @. storage = 2 * x
+end
+
+# pick feasible region
+# radius needs to be integer or rational
+lmo = FrankWolfe.ProbabilitySimplexOracle{Rational{BigInt}}(1)
+
+# compute some initial vertex
+x0 = FrankWolfe.compute_extreme_point(lmo, zeros(n));
+
+x, v, primal, dual_gap, trajectory = FrankWolfe.frank_wolfe(
+    f,
+    grad!,
+    lmo,
+    x0,
+    max_iteration=k,
+    line_search=FrankWolfe.Agnostic(),
+    print_iter=k / 10,
+    verbose=true,
+    emphasis=FrankWolfe.blas,
+);
+
+println("\nOutput type of solution: ", eltype(x))
 
 # Another possible step-size rule is `rationalshortstep` which computes the step size by minimizing the
 # smoothness inequality as ``\gamma_t=\frac{\langle \nabla f(x_t),x_t-v_t\rangle}{2L||x_t-v_t||^2}``. However, as this step size depends on an upper bound on the
 # Lipschitz constant ``L`` as well as the inner product with the gradient ``\nabla f(x_t)``, both have to be of a rational type.
+
+@time x, v, primal, dual_gap, trajectory = FrankWolfe.frank_wolfe(
+    f,
+    grad!,
+    lmo,
+    x0,
+    max_iteration=k,
+    line_search=FrankWolfe.RationalShortstep(),
+    L=2,
+    print_iter=k / 10,
+    verbose=true,
+    emphasis=FrankWolfe.blas,
+);
+
+# Note: at the last step, we exactly close the gap, finding the solution 1//n * ones(n)
