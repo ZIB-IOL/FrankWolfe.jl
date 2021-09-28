8d6b9b6814ac031ff6abdca5bed0355c358907e9
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-09-28 15:01:19 +0200

    [breaking] in-place stochastic Frank-Wolfe (#243)
    The stochastic objective function interface changed to be in-place, now expecting a partial gradient function
    `grad!(storage, x, data_point)` that adds the partial gradient wrt `data_point` onto storage **without** erasing it before.
    The sum is done in the `compute_gradient` function.
    
    * in-place stochastic Frank-Wolfe
    
    * better doc
    
    * fix example
    
    * copy with emphasis
    
    * test batch size logic
    
    * f name

--- test/function_gradient.jl
+++ test/function_gradient.jl
@@ -15,14 +15,20 @@ Random.seed!(123)
         c = rand(Float16)
         @testset "Simple function" begin
             simple_quad(x) = A * x ⋅ x / 2 + b ⋅ x + c
-            ∇simple_quad(x) = A * x + b
-            f_simple = FrankWolfe.SimpleFunctionObjective(simple_quad, ∇simple_quad)
+            function ∇simple_quad(storage, x)
+                storage .= A * x + b
+            end
+            f_simple = FrankWolfe.SimpleFunctionObjective(
+                simple_quad,
+                ∇simple_quad,
+                Vector{Float32}(undef, n), # storage
+            )
             for _ in 1:5
-                x = randn(n)
-                @test FrankWolfe.compute_gradient(f_simple, x) == ∇simple_quad(x)
+                x = randn(Float32, n)
+                @test FrankWolfe.compute_gradient(f_simple, x) == ∇simple_quad(similar(x), x)
                 @test FrankWolfe.compute_value(f_simple, x) == simple_quad(x)
                 @test FrankWolfe.compute_value_gradient(f_simple, x) ==
-                      (simple_quad(x), ∇simple_quad(x))
+                      (simple_quad(x), ∇simple_quad(similar(x), x))
                 @test FrankWolfe.compute_gradient(f_simple, convert(Vector{Float32}, x)) isa
                       Vector{Float32}
                 @test FrankWolfe.compute_value(f_simple, convert(Vector{Float32}, x)) isa Float32
@@ -35,12 +41,12 @@ Random.seed!(123)
                 pred = a ⋅ xi + b
                 return (pred - yi)^2 / 2
             end
-            function ∇simple_reg_loss(θ, data_point)
+            function ∇simple_reg_loss(storage, θ, data_point)
                 (xi, yi) = data_point
                 (a, b) = (θ[1:end-1], θ[end])
-                grad_a = xi * (a ⋅ xi + b - yi)
-                grad = push!(grad_a, a ⋅ xi + b - yi)
-                return grad
+                storage[1:end-1] .+= xi * (a ⋅ xi + b - yi)
+                storage[end] += a ⋅ xi + b - yi
+                return storage
             end
             xs = [10 * randn(5) for i in 1:10000]
             params = rand(6) .- 1 # start params in (-1,0)
@@ -49,11 +55,15 @@ Random.seed!(123)
             @testset "Perfectly representable data" begin
                 # Y perfectly representable by X
                 data_perfect = [(x, x ⋅ (1:5) + bias) for x in xs]
-                f_stoch =
-                    FrankWolfe.StochasticObjective(simple_reg_loss, ∇simple_reg_loss, data_perfect)
+                storage = similar(params_perfect)
+                f_stoch = FrankWolfe.StochasticObjective(
+                    simple_reg_loss, ∇simple_reg_loss, data_perfect, storage,
+                )
+                @test FrankWolfe._random_indices(f_stoch, 33, false)[1] == 33
+                @test FrankWolfe._random_indices(f_stoch, 33, true)[1] == length(xs)
                 @test compute_value(f_stoch, params) > compute_value(f_stoch, params_perfect)
                 @test compute_value(f_stoch, params_perfect) ≈ 0 atol = 1e-10
-                @test compute_gradient(f_stoch, params_perfect) ≈ zeros(6) atol = 6e-10
+                @test compute_gradient(f_stoch, params_perfect) ≈ zeros(6) atol = 1e-9
                 @test !isapprox(compute_gradient(f_stoch, params), zeros(6))
                 (f_estimate, g_estimate) = compute_value_gradient(
                     f_stoch,
@@ -84,8 +94,9 @@ Random.seed!(123)
             end
             @testset "Noisy data" begin
                 data_noisy = [(x, x ⋅ (1:5) + bias + 0.5 * randn()) for x in xs]
+                storage = similar(params_perfect)
                 f_stoch_noisy =
-                    FrankWolfe.StochasticObjective(simple_reg_loss, ∇simple_reg_loss, data_noisy)
+                    FrankWolfe.StochasticObjective(simple_reg_loss, ∇simple_reg_loss, data_noisy, storage)
                 @test compute_value(f_stoch_noisy, params) >
                       compute_value(f_stoch_noisy, params_perfect)
                 # perfect parameters shouldn't have too high of a residual gradient
