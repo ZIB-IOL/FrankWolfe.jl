6d43822697fca247ca259416e078cfdd30768947
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-10-11 11:11:10 +0200

    Apply suggestions from code review

--- src/polytope_oracles.jl
+++ src/polytope_oracles.jl
@@ -135,8 +135,8 @@ end
     ScaledBoundLInfNormBall(lower_bounds, upper_bounds)
 
 Polytope similar to a L-inf-ball with shifted bounds or general box constraints.
-Lower and upper bounds are passed on as abstract vectors, possibly of different types.
-For the standard L-inf-ball, i.e. the standard hypercube, all lower and upper bounds would be -1 and 1.
+Lower- and upper-bounds are passed on as abstract vectors, possibly of different types.
+For the standard L-inf-ball, all lower- and upper-bounds would be -1 and 1.
 """
 struct ScaledBoundLInfNormBall{T, VT1 <: AbstractVector{T}, VT2 <: AbstractVector{T}} <: LinearMinimizationOracle
     lower_bounds::VT1
