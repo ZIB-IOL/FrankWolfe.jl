936df8e1d2e8f3afd06ad42d976d2e594728cc9e
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-10-27 12:23:41 +0200

    reorganize docs, extended exemple

--- docs/src/examples/6_spectrahedron.jl
+++ docs/src/examples/6_spectrahedron.jl
@@ -1,10 +1,17 @@
 # # Spectrahedron
-
-# This example shows an example optimizing over the spectraplex:
+#
+# This example shows an optimization problem over the spectraplex:
 # ```math
 # S = \{X \in \mathbb{S}_+^n, Tr(X) = 1\}
 # ```
-
+# with $\mathbb{S}_+^n$ the set of positive semidefinite matrices.
+# Linear optimization with symmetric objective $D$ over the spetraplex consists in computing the
+# leading eigenvector of $D$.
+#
+# The package also exposes `UnitSpectrahedronLMO` which corresponds to the feasible set:
+# ```math
+# S_u = \{X \in \mathbb{S}_+^n, Tr(X) \leq 1\}
+# ```
 
 using FrankWolfe
 using LinearAlgebra
@@ -16,7 +23,9 @@ using SparseArrays
 # f(X) = \sum_{(i,j) \in L} 1/2 (X_{ij} - Y_{ij})^2
 # ```
 
-# dimension, number of iterations and number of known entries
+# ## Setting up the input data, objective, and gradient
+
+# Dimension, number of iterations and number of known entries:
 n = 500
 k = 10000
 n_entries = 50
@@ -46,14 +55,15 @@ end
 const lmo = FrankWolfe.SpectraplexLMO(1.0, n)
 const x0 = FrankWolfe.compute_extreme_point(lmo, spzeros(n, n))
 
-target_tolerance = 1e-6
+target_tolerance = 1e-6;
 
 #src the following two lines are used only to precompile the functions
 FrankWolfe.frank_wolfe(f, grad!, lmo, x0, max_iteration=2, line_search=FrankWolfe.MonotonousStepSize()) #src
 FrankWolfe.lazified_conditional_gradient(f, grad!, lmo, x0, max_iteration=2, line_search=FrankWolfe.MonotonousStepSize()) #src
 
-# Running standard and lazified Frank-Wolfe
-# Note the `ensure_symmetry` keyword argument passed to the SpectraplexLMO extreme point computation.
+# ## Running standard and lazified Frank-Wolfe
+#
+# Note that the `ensure_symmetry` keyword argument passed to the SpectraplexLMO extreme point computation.
 # It skips an additional step making the used direction symmetric.
 # It is not necessary when the gradient is a `LinearAlgebra.Symmetric` (or more rarely a `LinearAlgebra.Diagonal` or `LinearAlgebra.UniformScaling`).
 
@@ -86,6 +96,8 @@ Xfinal, Vfinal, primal, dual_gap, trajectory_lazy = FrankWolfe.lazified_conditio
     epsilon=target_tolerance,
     ensure_symmetry=false,
 )
+
+# ## Plotting the resulting trajectories
 
 data = [trajectory, trajectory_lazy]
 label = ["FW", "LCG"]
