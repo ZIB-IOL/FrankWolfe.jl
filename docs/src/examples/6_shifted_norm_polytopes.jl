7fceb4b1a0f779532b83e2e1e94a157bdfc40bb1
j-geuter <85136388+j-geuter@users.noreply.github.com>
2021-11-01 16:15:22 +0100

    Balls fixes (#256)
    * basic documentation structure
    
    * docs/build in gitignore now
    
    * docs/build/ in .gitignore now
    
    * docs/build/ added to .gitignore
    
    * dollarsigns replaced
    
    * Update ci.yml
    
    * Project.toml update
    
    * docs/Project.toml added
    
    * minor fix
    
    * fixes for balls_example

--- docs/src/examples/6_shifted_norm_polytopes.jl
+++ docs/src/examples/6_shifted_norm_polytopes.jl
@@ -3,9 +3,9 @@ using LinearAlgebra
 using LaTeXStrings
 using Plots
 
-# # FrankWolfe for scaled, shifted ``L^1`` and ``L^{\infty}`` norm balls
+# # FrankWolfe for scaled, shifted ``\ell^1`` and ``\ell^{\infty}`` norm balls
 
-# In this example, we run the vanilla FrankWolfe algorithm on a scaled and shifted ``L^1`` and ``L^{\infty}`` norm ball, using the `ScaledBoundL1NormBall`
+# In this example, we run the vanilla FrankWolfe algorithm on a scaled and shifted ``\ell^1`` and ``\ell^{\infty}`` norm ball, using the `ScaledBoundL1NormBall`
 # and `ScaledBoundLInfNormBall` LMOs. We shift both onto the point ``(1,0)`` and then scale them by a factor of ``2`` along the x-axis. We project the point ``(2,1)`` onto the polytopes.
 
 n = 2
@@ -45,7 +45,7 @@ x_l1, v_1, primal_1, dual_gap_1, trajectory_1 = FrankWolfe.frank_wolfe(
     trajectory=true,
 );
 
-println("\nFinal approximation: ", x_l1)
+println("\nFinal solution: ", x_l1)
 
 x2 = FrankWolfe.compute_extreme_point(linf, zeros(n))
 gradient = collect(x2)
@@ -64,10 +64,10 @@ x_linf, v_2, primal_2, dual_gap_2, trajectory_2 = FrankWolfe.frank_wolfe(
     trajectory=true,
 );
 
-println("\nFinal approximation: ", x_linf)
+println("\nFinal solution: ", x_linf)
 
 
-# We plot the polytopes alongside the approximative solutions from above:
+# We plot the polytopes alongside the solutions from above:
 
 xcoord1 = [1,3,1,-1,1]
 ycoord1 = [-1,0,1,0,-1]
@@ -75,7 +75,7 @@ ycoord1 = [-1,0,1,0,-1]
 xcoord2 = [3,3,-1,-1,3]
 ycoord2 = [-1,1,1,-1,-1]
 
-plot(xcoord1, ycoord1, title = "Visualization of scaled shifted norm balls", lw = 2, label = L"L^1 \textrm{ norm}")
-plot!(xcoord2, ycoord2, lw = 2, label = L"L^{\infty} \textrm{ norm}")
-plot!([x_l1[1]], [x_l1[2]], seriestype = :scatter, lw = 5, color = "blue", label = L"L^1 \textrm{ approximation}")
-plot!([x_linf[1]], [x_linf[2]], seriestype = :scatter, lw = 5, color = "orange", label = L"L^{\infty} \textrm{ approximation}", legend = :bottomleft)
+plot(xcoord1, ycoord1, title = "Visualization of scaled shifted norm balls", lw = 2, label = L"\ell^1 \textrm{ norm}")
+plot!(xcoord2, ycoord2, lw = 2, label = L"\ell^{\infty} \textrm{ norm}")
+plot!([x_l1[1]], [x_l1[2]], seriestype = :scatter, lw = 5, color = "blue", label = L"\ell^1 \textrm{ solution}")
+plot!([x_linf[1]], [x_linf[2]], seriestype = :scatter, lw = 5, color = "orange", label = L"\ell^{\infty} \textrm{ solution}", legend = :bottomleft)
