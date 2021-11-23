1c248d6f918633cb3423c120b53cf1a2644cd535
j-geuter <jonathan.geuter@gmx.de>
2021-11-23 09:04:33 -0500

    add sentence to docs

--- src/oracles.jl
+++ src/oracles.jl
@@ -12,6 +12,7 @@ abstract type LinearMinimizationOracle end
 
 Computes the point `argmin_{v ∈ C} v ⋅ direction`
 with `C` the set represented by the LMO.
+Most LMOs feature `v` as a keyword argument that allows for an in-place computation whenever `v` is dense.
 All LMOs should accept keyword arguments that they can ignore.
 """
 function compute_extreme_point end
