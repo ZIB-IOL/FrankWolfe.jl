d36907f75b0525a35c059af8325b49c64dbeeeed
j-geuter <jonathan.geuter@gmx.de>
2021-12-08 17:24:52 -0500

    tests update

--- test/in_place_performance.jl
+++ test/in_place_performance.jl
@@ -8,25 +8,46 @@ function grad!(storage, x)
     return storage .= 2x
 end
 
-lmos = [FrankWolfe.ProbabilitySimplexOracle(1),
-        FrankWolfe.ProbabilitySimplexOracle(1),
-        FrankWolfe.ProbabilitySimplexOracle(1),
+lmos = [
         FrankWolfe.ScaledBoundLInfNormBall(-ones(10), 2 * ones(10)),
         FrankWolfe.ScaledBoundLInfNormBall(-2 * ones(10), 2 * ones(10)),
+        FrankWolfe.ScaledBoundLInfNormBall(-ones(100), 2 * ones(100)),
+        FrankWolfe.ScaledBoundLInfNormBall(-2 * ones(100), 2 * ones(100)),
         FrankWolfe.ScaledBoundL1NormBall(-ones(10), 2 * ones(10)),
         FrankWolfe.ScaledBoundL1NormBall(-2 * ones(10), 2 * ones(10)),
         FrankWolfe.KNormBallLMO(5, 2 * pi),
-        FrankWolfe.KNormBallLMO(10, 2 * pi)
+        FrankWolfe.KNormBallLMO(10, 2 * pi),
+        FrankWolfe.KNormBallLMO(10, 2 * pi),
+        FrankWolfe.LpNormLMO{1.5}(5),
+        FrankWolfe.LpNormLMO{1.5}(50),
+        FrankWolfe.LpNormLMO{1.5}(50),
+        FrankWolfe.LpNormLMO{2}(5),
+        FrankWolfe.LpNormLMO{2}(50),
+        FrankWolfe.LpNormLMO{2}(50),
+        FrankWolfe.LpNormLMO{Inf}(5),
+        FrankWolfe.LpNormLMO{Inf}(50),
+        FrankWolfe.LpNormLMO{Inf}(50),
         ]
-d =    [zeros(5),
-        zeros(50),
-        zeros(200),
+d =    [
+        zeros(10),
+        zeros(10),
+        zeros(100),
+        zeros(100),
         zeros(10),
         zeros(10),
         zeros(10),
         zeros(10),
+        zeros(100),
         zeros(10),
-        zeros(10)]
+        zeros(10),
+        zeros(100),
+        zeros(10),
+        zeros(10),
+        zeros(100),
+        zeros(10),
+        zeros(10),
+        zeros(100),
+        ]
 
 for i in 1:length(d)
         FrankWolfe.frank_wolfe(f, grad!, lmos[i], FrankWolfe.compute_extreme_point(lmos[i], d[i]))
