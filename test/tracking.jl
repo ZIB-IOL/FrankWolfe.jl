bec84a2bc7551d8af64fe468ae3f21b7b380ef72
dviladrich95 <daniel.viladrich.h@gmail.com>
2021-12-23 15:21:23 +0100

    added packages for fw_algorithms testing with tracking added additional tests for tracking objects added SUITE structure and first FW test from test/runtests

--- test/tracking.jl
+++ test/tracking.jl
@@ -1,35 +1,96 @@
 using FrankWolfe
 using Test
+using LinearAlgebra
 
-f(x) = norm(x)^2
-function grad!(storage, x)
-    return storage .= 2x
+using DoubleFloats
+using DelimitedFiles
+import FrankWolfe: ActiveSet
+
+
+@testset "Tracking Testset" begin
+    f(x) = norm(x)^2
+    function grad!(storage, x)
+        return storage .= 2x
+    end
+    
+    x = zeros(6)
+    gradient = similar(x)
+    rhs = 10 * rand()
+    lmo = FrankWolfe.ProbabilitySimplexOracle(rhs)
+    direction = zeros(6)
+    direction[1] = -1
+    
+    @testset "TrackingGradient" begin
+        tgrad! = FrankWolfe.TrackingGradient(grad!)
+        @test tgrad!.counter == 0
+        @test tgrad!.grad! === grad!
+        tgrad!(gradient,direction)
+        @test tgrad!.counter == 1
+    
+    
+    end
+    
+    @testset "TrackingObjective" begin
+        tf = FrankWolfe.TrackingObjective(f,0)
+        @test tf.counter == 0
+        tf(x)
+        @test tf.counter == 1
+    
+    end
+    
+    @testset "TrackingLMO" begin
+        tlmo_prob = FrankWolfe.TrackingLMO(lmo,0)
+        @test tlmo_prob.counter == 0
+        @test tlmo_prob.lmo ===  lmo
+        compute_extreme_point(tlmo_prob,direction)
+        @test tlmo_prob.counter == 1
+    end
 end
 
-x = zeros(6)
-gradient = similar(x)
-rhs = 10 * rand()
-direction = zeros(6)
-direction[1] = -1
 
-@testset "TrackingGradient" begin
-    tg = FrankWolfe.TrackingGradient(grad!)
-    @test tg.counter == 0
-    @test tg.grad! === grad!
-    tg(gradient,direction)
 
-end
+SUITE = Dict()
 
-@testset "TrackingObjective" begin
-    to = FrankWolfe.TrackingObjective(f,0)
-    @test to.counter == 0
+SUITE["vanilla_fw"] = Dict()
+SUITE["lazified_cd"] = Dict()
+SUITE["blas_vs_memory"] = Dict()
+SUITE["dense_structure"] = Dict()
+SUITE["rational"] = Dict()
+SUITE["multi_precision"] = Dict()
+SUITE["stochastic_fw"] = Dict()
+SUITE["away_step_fw"] = Dict()
+SUITE["blended_cg"] = Dict()
 
-end
+# "Testing vanilla Frank-Wolfe with various step size and momentum strategies" begin
+    f(x) = norm(x)^2
+    tf = FrankWolfe.TrackingObjective(f,0)
 
-@testset "TrackingLMO" begin
-    tlmo_prob = FrankWolfe.TrackingLMO(FrankWolfe.ProbabilitySimplexOracle(rhs),0)
-    @test tlmo_prob.counter == 0
-    compute_extreme_point(tlmo_prob,direction)
-    @test tlmo_prob.counter == 1
-end
+    function grad!(storage, x)
+        return storage .= 2x
+    end
+    tgrad! = FrankWolfe.TrackingGradient(grad!,0)
+    
+    lmo_prob = FrankWolfe.ProbabilitySimplexOracle(1)
+    tlmo_prob = FrankWolfe.TrackingLMO(lmo_prob)
+
+    x0 = FrankWolfe.compute_extreme_point(tlmo_prob, zeros(5))
+    # f_values, dual_values, function_calls, gradient_calls, lmo_calls, time_vec
+    tracking_callback = FrankWolfe.build_tracking_callback([],[],[],[],[],[])
+
+    SUITE["vanilla_fw"]["1"] = FrankWolfe.frank_wolfe(
+            tf,
+            tgrad!,
+            tlmo_prob,
+            x0,
+            max_iteration=1000,
+            line_search=FrankWolfe.Agnostic(),
+            trajectory=true,
+            callback=tracking_callback,
+            verbose=false,
+    )
+
+    x, v, primal, dual_gap, trajectory = SUITE["vanilla_fw"]["1"]
+
+    #println(trajectory)
+
 
