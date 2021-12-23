c62674cca11ba8244e51cd2a4b2d8fdcf741a12d
dviladrich95 <daniel.viladrich.h@gmail.com>
2021-12-23 21:45:26 +0100

    Changed TrackingLMO Abstract Type added documentation

--- src/tracking.jl
+++ src/tracking.jl
@@ -1,3 +1,8 @@
+"""
+    TrackingGradient{G}
+    A mutable struct that can act like the normal
+    grad! function but with an additional method for count tracking
+"""
 mutable struct TrackingGradient{G} <: Function
     grad!::G
     counter::Int
@@ -11,6 +16,11 @@ function (tg::TrackingGradient)(storage, x)
 end
 
 
+"""
+    TrackingObjective{F}
+    A mutable struct that can act like the normal
+    objective function f but with an additional method for count tracking
+"""
 mutable struct TrackingObjective{F} <: Function
     f::F
     counter::Int
@@ -23,11 +33,24 @@ function (tf::TrackingObjective)(x)
     return tf.f(x)
 end
 
-mutable struct TrackingLMO{LMO} <: Function
+"""
+    TrackingLMO{LMO}
+    A mutable struct that can act like the normal
+    LMO oracle but with an additional method for count tracking
+"""
+mutable struct TrackingLMO{LMO} <: LinearMinimizationOracle
     lmo::LMO
     counter::Int
 end
 
+"""
+    compute_extreme_point(lmo::TrackingLMO, direction; kwargs...)
+    Mirrors behaviour of same function found in oracles.jl, increasing
+    call counter
+Computes the point `argmin_{v ∈ C} v ⋅ direction`
+with `C` the set represented by the LMO.
+All LMOs should accept keyword arguments that they can ignore.
+"""
 function compute_extreme_point(lmo::TrackingLMO, x; kwargs...)
     lmo.counter += 1
     return compute_extreme_point(lmo.lmo, x)
@@ -38,6 +61,13 @@ is_tracking_lmo(lmo::TrackingLMO) = true
 
 TrackingLMO(lmo) = TrackingLMO(lmo, 0)
 
+"""
+    tracking_trajectory_callback(storage)
+    similar to trajectory_callback found in utils.jl
+Callback pushing the state at each iteration to the passed storage.
+The state data is only the 5 first fields + 3 call counters usually:
+`(t,primal,dual,dual_gap,time,function_calls,gradient_calls,lmo_calls)`
+"""
 function tracking_trajectory_callback(storage)
     return function tracking_push_trajectory!(state)
         return push!(storage, Tuple(state)[1:8])
