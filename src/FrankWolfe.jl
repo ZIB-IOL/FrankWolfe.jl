ecc25f6fc019a09a22d23b237ae46a4cd9018b48
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-02-09 00:13:14 +0100

    added tracking src to FrankWolfe.jl

--- src/FrankWolfe.jl
+++ src/FrankWolfe.jl
@@ -39,6 +39,7 @@ include("blended_cg.jl")
 include("afw.jl")
 include("fw_algorithms.jl")
 include("pairwise.jl")
+include("tracking.jl")
 
 # collecting most common data types etc and precompile 
 # min version req set to 1.5 to prevent stalling of julia 1
