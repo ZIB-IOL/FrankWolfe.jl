91ac936b48354d60c70ccf92608c1ff4130d93a8
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-02-25 14:11:45 +0100

    added PkgBenchmark functions for branch changing

--- src/tracking.jl
+++ src/tracking.jl
@@ -95,3 +95,31 @@ function tracking_cached_trajectory_callback(storage)
         return push!(storage, complete_tuple)
     end
 end
+
+
+"""
+
+Runs a function at a commit on a repo and afterwards goes back
+to the original commit / branch.
+"""
+function withcommit(f, repo, commit)
+    original_commit = shastring(repo, "HEAD")
+    LibGit2.transact(repo) do r
+        branch = try LibGit2.branch(r) catch err; nothing end
+        try
+            LibGit2.checkout!(r, shastring(r, commit))
+            f()
+        catch err
+            rethrow(err)
+        finally
+            if branch !== nothing
+                LibGit2.branch!(r, branch)
+            else
+                LibGit2.checkout!(r, original_commit)
+            end
+        end
+    end
+end
+
+shastring(r::LibGit2.GitRepo, targetname) = string(LibGit2.revparseid(r, targetname))
+shastring(dir::AbstractString, targetname) = LibGit2.with(r -> shastring(r, targetname), LibGit2.GitRepo(dir))
