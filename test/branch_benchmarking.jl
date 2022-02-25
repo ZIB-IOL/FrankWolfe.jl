586da0050293b64d677e147b627da318be3ac29c
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-02-25 12:23:14 +0100

    print tests

--- test/branch_benchmarking.jl
+++ test/branch_benchmarking.jl
@@ -7,15 +7,18 @@ using LibGit2
 import FrankWolfe: ActiveSet
 using PkgBenchmark
 
-include("benchmarking_suite.jl")
 
-function get_head_shastring()
-    repo_dir = pwd()
-    repo = LibGit2.GitRepo(repo_dir)
-    commit_head = LibGit2.peel(LibGit2.GitCommit,LibGit2.head(repo))
-    shastring_head = LibGit2.GitHash(commit_head)
-    return shastring_head
+function run_include()
+    read("benchmarking_suite.jl")
 end
+
+# function get_head_shastring()
+#     repo_dir = pwd()
+#     repo = LibGit2.GitRepo(repo_dir)
+#     commit_head = LibGit2.peel(LibGit2.GitCommit,LibGit2.head(repo))
+#     shastring_head = LibGit2.GitHash(commit_head)
+#     return shastring_head
+# end
 
 suite=Dict()
 
@@ -24,7 +27,7 @@ repo_base = LibGit2.GitRepo(dir_base)
 commit_base = LibGit2.peel(LibGit2.GitCommit,LibGit2.head(repo_base))
 shastring_base = string(LibGit2.GitHash(commit_base))
 
-suite[shastring_base]=run_benchmark()
+# suite[shastring_base]=run_benchmark()
 
 
 shastring_branch = "2d262639d02bc1a6bb5d1ed286160a0c96b0f5cc"
@@ -33,7 +36,7 @@ shastring_branch = "2d262639d02bc1a6bb5d1ed286160a0c96b0f5cc"
 
 println(suite[shastring_base][end])
 
-suite[shastring_branch] = PkgBenchmark._withcommit(run_benchmark, repo_base,shastring_branch)
+suite[shastring_branch] = PkgBenchmark._withcommit(run_include, repo_base,shastring_branch)
 
-println(suite[shastring_base][end][end])
-println(suite[shastring_branch][end][end])
+# println(suite[shastring_base][end][end])
+# println(suite[shastring_branch][end][end])
