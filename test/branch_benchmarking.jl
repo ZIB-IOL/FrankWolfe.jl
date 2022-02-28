e68d2e4362b665b7b4a171fe4ad5e2234ed44a4f
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-02-28 16:56:29 +0100

    Added testset

--- test/branch_benchmarking.jl
+++ test/branch_benchmarking.jl
@@ -15,28 +15,28 @@ function get_include(dir)
     end
 end
 
+@testset "Branch Benchmarking" begin
+
+    suite=Dict()
+    dir_base = pwd()
+    run_include = get_include(dir_base)
+
+    repo_base = LibGit2.GitRepo(dir_base)
+    commit_base = LibGit2.peel(LibGit2.GitCommit,LibGit2.head(repo_base))
+    shastring_base = string(LibGit2.GitHash(commit_base))
+
+    suite[shastring_base] = run_include()
 
 
-suite=Dict()
-dir_base = pwd()
-run_include = get_include(dir_base)
+    shastring_branch = "2d262639d02bc1a6bb5d1ed286160a0c96b0f5cc"
 
-repo_base = LibGit2.GitRepo(dir_base)
-commit_base = LibGit2.peel(LibGit2.GitCommit,LibGit2.head(repo_base))
-shastring_base = string(LibGit2.GitHash(commit_base))
+    # function, grad! and lmo counters
+    # println(suite[shastring_base][end])
 
-suite[shastring_base] = run_include()
+    suite[shastring_branch] = FrankWolfe.withcommit(run_include, repo_base,shastring_branch)
 
+    @test suite[shastring_base][end][end] ==  2502
+    @test suite[shastring_branch][end][end] == 5002
 
-shastring_branch = "2d262639d02bc1a6bb5d1ed286160a0c96b0f5cc"
-
-# function, grad! and lmo counters
-# println(suite[shastring_base][end])
-
-suite[shastring_branch] = FrankWolfe.withcommit(run_include, repo_base,shastring_branch)
-
-# @test suite[shastring_base][end][end] =  2502
-# @test suite[shastring_branch][end][end] = 5002
-
-
+end
 
