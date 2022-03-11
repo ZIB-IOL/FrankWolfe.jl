98140b06f930b9f1b3245e876204b66cd3609386
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-03-11 11:53:36 +0100

    remove "!" because not modifying anymore

--- src/stop_criterion.jl
+++ src/stop_criterion.jl
@@ -5,7 +5,7 @@ Callback checking if the primal objective is surpasses the value f_limit.
 If so, sets the stop_criterion to true 
 """
 function primal_value_callback(primal_limit)
-    return function limit_check!(state) 
+    return function limit_check(state) 
         return state.primal < primal_limit
     end
 end
@@ -17,7 +17,7 @@ Callback checking if the gradient call count surpasses the value grad_limit.
 If so, sets the stop_criterion to true 
 """
 function f_call_callback(f_call_limit)
-    return function limit_check!(state) 
+    return function limit_check(state) 
         return state.f.counter < f_call_limit
     end
 end
@@ -29,7 +29,7 @@ Callback checking if the gradient call count surpasses the value grad_limit.
 If so, sets the stop_criterion to true 
 """
 function grad_call_callback(grad_call_limit)
-    return function limit_check!(state) 
+    return function limit_check(state) 
         return state.grad.counter < grad_call_limit
     end
 end
@@ -41,7 +41,7 @@ Callback checking if the lmo call count surpasses the value lmo_limit.
 If so, sets the stop_criterion to true 
 """
 function lmo_call_callback(lmo_call_limit)
-    return function limit_check!(state) 
+    return function limit_check(state) 
         return state.lmo.counter < lmo_call_limit
     end
 end
