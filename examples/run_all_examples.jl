30f599a60e48c16c1ef4ccaed7cd192630788688
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2022-03-15 19:33:23 +0100

    Fix CI for examples, hopefully for the last time

--- examples/run_all_examples.jl
+++ examples/run_all_examples.jl
@@ -1,7 +1,5 @@
 using Pkg
 Pkg.activate(dirname(@__DIR__))
-Pkg.instantiate()
-
 using TestEnv
 TestEnv.activate()
 
@@ -10,13 +8,13 @@ using Random
 # for bug with display
 ENV["GKSwstype"] = "100"
 
-example_files = filter(readdir(@__DIR__, join=true)) do f
+example_files = filter(readdir(@__DIR__; join=true)) do f
     endswith(f, ".jl") && !occursin("large", f) && !occursin("result", f) && f != "activate.jl"
 end
 
 example_shuffle = randperm(length(example_files))
 
 for file in example_files[example_shuffle[1:2]]
-    @info "running example $file"
-    run(`julia $file`)
+    @info "Including example $file"
+    include(file)
 end
