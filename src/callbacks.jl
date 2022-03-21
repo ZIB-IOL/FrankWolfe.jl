6d8456d51eba842a65feac97aa8356aa45d3adae
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-03-21 11:12:50 +0100

    moved all callbacks to new callbacks file

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -45,3 +45,37 @@ function lmo_call_callback(lmo_call_limit)
         return state.lmo.counter < lmo_call_limit
     end
 end
+
+"""
+A function acting like the passed callback,
+    but adding the state to the storage variable.
+    The state data is only the 5 first fields, gamma and 3 call counters, usually
+`(t, primal, dual, dual_gap, time, gamma, function_calls, gradient_calls, lmo_calls)`
+"""
+function tracking_callback(storage, callback)
+    function push_and_callback(state)
+        base_tuple = Tuple(state)[1:5]
+        complete_tuple = tuple(base_tuple..., state.gamma, state.f.counter, state.grad.counter, state.lmo.counter)
+        push!(storage, complete_tuple)
+        return callback(state)
+    end
+end
+
+tracking_callback(storage) = tracking_callback(storage, state->false)
+
+"""
+A function acting like the passed callback for cached LMOs,
+    but adding the state to the storage variable.
+    The state data is only the 5 first fields, gamma and 3 call counters, usually
+`(t, primal, dual, dual_gap, time, gamma, function_calls, gradient_calls, lmo_calls)`
+"""
+function tracking_cached_callback(storage, callback)
+    function push_and_callback(state)
+        base_tuple = Tuple(state)[1:5]
+        complete_tuple = tuple(base_tuple..., state.gamma, state.f.counter, state.grad.counter, state.lmo.inner.counter)
+        push!(storage, complete_tuple)
+        return callback(state)
+    end
+end
+
+tracking_cached_callback(storage) = tracking_cached_callback(storage, state->false)
