fae9ad284ea49701e0d992d2e73694ee9e87834e
Jonathan Geuter <jonathan.geuter@gmx.de>
2022-04-21 15:37:38 +0200

    add CallbackState struct

--- src/defs.jl
+++ src/defs.jl
@@ -39,3 +39,32 @@ const st = (
     last="Last",
     pp="PP",
 )
+
+
+struct CallbackState{XT,VT,FT,GT,LMO<:LinearMinimizationOracle}
+    t::Int64
+    primal::Float64
+    dual::Float64
+    dual_gap::Float64
+    time::Float64
+    x::XT
+    v::VT
+    gamma::Float64
+    f::FT
+    grad!::GT
+    lmo::LMO
+    gradient::XT
+end
+
+struct CallbackActiveSetState{AT <: ActiveSet}
+    base_state::CallbackState
+    active_set::AT
+end
+
+function Base.getproperty(state::CallbackActiveSetState, f::Symbol)
+    if f === :active_set
+        return getfield(state, f)
+    end
+    base_state = getfield(state, :base_state)
+    return getproperty(base_state, f)
+end
