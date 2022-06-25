e473335a3ff7cda151ee6b4d37e6e245294f0e5c
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-06-25 16:58:47 +0200

    vertex, broadcast

--- src/afw.jl
+++ src/afw.jl
@@ -270,7 +270,7 @@ function away_frank_wolfe(
     tt = pp
     tot_time= (time_ns()- time_start) / 1e9
     if callback !== nothing 
-        state = CallbackState(t, primal, primal-dual_gap, dual_gap, tot_time, x, vertex, gamma, f, lmo, gradient, tt)
+        state = CallbackState(t, primal, primal-dual_gap, dual_gap, tot_time, x, v, gamma, f, lmo, gradient, tt)
         callback(state, active_set)
     end
 
