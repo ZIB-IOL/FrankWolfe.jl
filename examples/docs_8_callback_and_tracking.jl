767370a2187b0dd3722e6bb50967116344039901
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-06-25 19:06:57 +0200

    base tuple

--- examples/docs_8_callback_and_tracking.jl
+++ examples/docs_8_callback_and_tracking.jl
@@ -82,7 +82,7 @@ storage = []
 # the five default logged elements `(iterations, primal, dual, dual_gap, time)` with ".counter" field arguments present in the tracking functions.
 
 function push_tracking_state(state, storage)
-    base_tuple = Tuple(state)[1:5]
+    base_tuple = FrankWolfe.callback_state(state)
     if typeof(state.lmo) <: FrankWolfe.CachedLinearMinimizationOracle
         complete_tuple = tuple(base_tuple..., state.gamma, state.f.counter, state.grad.counter, state.lmo.inner.counter)
     else
