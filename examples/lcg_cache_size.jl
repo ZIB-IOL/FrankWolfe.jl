e886e7c55a3c5efcc06df1896c29bb0df02effee
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-11-07 10:18:37 +0100

    improve example

--- examples/lcg_cache_size.jl
+++ examples/lcg_cache_size.jl
@@ -2,8 +2,6 @@ using FrankWolfe
 using ProgressMeter
 using Arpack
 using Plots
-using DoubleFloats
-using ReverseDiff
 
 using LinearAlgebra
 
@@ -14,33 +12,15 @@ xpi = rand(n);
 total = sum(xpi);
 const xp = xpi # ./ total;
 
-f(x) = norm(x - xp)^2
+f(x) = dot(x, x) - 2 * dot(x, xp)
 function grad!(storage, x)
     @. storage = 2 * (x - xp)
-end
-
-# better for memory consumption as we do coordinate-wise ops
-
-function cf(x, xp)
-    return LinearAlgebra.norm(x .- xp)^2
-end
-
-function cgrad!(storage, x, xp)
-    return @. storage = 2 * (x - xp)
 end
 
 # lmo = FrankWolfe.ProbabilitySimplexOracle(1);
 
 lmo = FrankWolfe.KSparseLMO(100, 1.0)
 x00 = FrankWolfe.compute_extreme_point(lmo, zeros(n));
-
-FrankWolfe.benchmark_oracles(
-    x -> cf(x, xp),
-    (str, x) -> cgrad!(str, x, xp),
-    () -> randn(n),
-    lmo;
-    k=100,
-)
 
 # arbitrary cache
 
@@ -64,8 +44,8 @@ x0 = deepcopy(x00)
 x0 = deepcopy(x00)
 
 @time x, v, primal, dual_gap, trajectory = FrankWolfe.lazified_conditional_gradient(
-    x -> cf(x, xp),
-    (str, x) -> cgrad!(str, x, xp),
+    f,
+    grad!,
     lmo,
     x0,
     max_iteration=k,
