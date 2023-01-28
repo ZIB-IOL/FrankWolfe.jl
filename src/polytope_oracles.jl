b97c6cdeece234aeacee23fd9f8aac69cec0eb38
Mathieu Besançon <mathieu.besancon@gmail.com>
2023-01-28 17:36:16 +0100

    generic array linf

--- src/polytope_oracles.jl
+++ src/polytope_oracles.jl
@@ -142,7 +142,7 @@ Polytope similar to a L-inf-ball with shifted bounds or general box constraints.
 Lower- and upper-bounds are passed on as abstract vectors, possibly of different types.
 For the standard L-inf ball, all lower- and upper-bounds would be -1 and 1.
 """
-struct ScaledBoundLInfNormBall{T,VT1<:AbstractVector{T},VT2<:AbstractVector{T}} <:
+struct ScaledBoundLInfNormBall{T,N,VT1<:AbstractArray{T,N},VT2<:AbstractArray{T,N}} <:
        LinearMinimizationOracle
     lower_bounds::VT1
     upper_bounds::VT2
@@ -154,7 +154,7 @@ function compute_extreme_point(
     v=similar(lmo.lower_bounds),
     kwargs...,
 )
-    v .= lmo.lower_bounds
+    copyto!(v, lmo.lower_bounds)
     for i in eachindex(direction)
         if direction[i] * lmo.upper_bounds[i] < direction[i] * lmo.lower_bounds[i]
             v[i] = lmo.upper_bounds[i]
