e59c8314b2c1d6400f59755b33354c13966ce565
Mathieu Besançon <mathieu.besancon@gmail.com>
2023-04-19 09:44:35 +0200

    added ellipsoid (#393)
    * added ellipsoid
    
    * order of imports
    
    * import JuMP
    
    * larger tol
    
    * inf norm
    
    * precision
    
    * precision
    
    * efficient versoin

--- test/lmo.jl
+++ test/lmo.jl
@@ -2,16 +2,17 @@ using Test
 using FrankWolfe
 using LinearAlgebra
 import SparseArrays
+using Random
 
 import FrankWolfe: compute_extreme_point, LpNormLMO, KSparseLMO
 
-import GLPK
-import MathOptInterface
-const MOI = MathOptInterface
+import MathOptInterface as MOI
 
+# solvers
+import GLPK
 import Clp
-using Random
 import Hypatia
+using JuMP
 
 @testset "Simplex LMOs" begin
     n = 6
@@ -750,4 +751,36 @@ end
     x_dense, _, _, _, _ = FrankWolfe.frank_wolfe(fun0, fun0_grad!, lmo_dense, [1.0, 0.0, 0.0])
     x_standard, _, _, _, _ = FrankWolfe.frank_wolfe(fun0, fun0_grad!, lmo_standard, [1.0, 0.0, 0.0])
     @test x_dense == x_standard
+end
+
+@testset "Ellipsoid LMO $n" for n in (2, 5, 10)
+    A = zeros(n, n)
+    A[1,1] = 3
+    @test_throws PosDefException FrankWolfe.EllipsoidLMO(A)
+    for i in 1:n
+        A[i,i] = 3
+    end
+    radius = 4 * rand()
+    center = randn(n)
+    lmo = FrankWolfe.EllipsoidLMO(A, center, radius)
+    d = randn(n)
+    v = FrankWolfe.compute_extreme_point(lmo, d)
+    @test dot(v - center, A, v - center) ≈ radius atol=1e-10
+    A = randn(n,n)
+    A += A'
+    while !isposdef(A)
+        A += I
+    end
+    lmo = FrankWolfe.EllipsoidLMO(A, center, radius)
+    d = randn(n)
+    v = FrankWolfe.compute_extreme_point(lmo, d)
+    @test dot(v - center, A, v - center) ≈ radius atol=1e-10
+    m = Model(Hypatia.Optimizer)
+    @variable(m, x[1:n])
+    @constraint(m, dot(x-center, A, x-center) ≤ radius)
+    @objective(m, Min, dot(x, d))
+    JuMP.set_silent(m)
+    optimize!(m)
+    xv = JuMP.value.(x)
+    @test norm(xv - v, Inf) <= 2n*1e-6
 end
