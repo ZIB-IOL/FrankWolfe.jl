ca4b6349a822c22b8d50d8f3d96ba9890d814ab5
Mathieu Besançon <mathieu.besancon@gmail.com>
2023-07-04 11:11:39 +0200

    fixed check

--- examples/plot_utils.jl
+++ examples/plot_utils.jl
@@ -389,7 +389,8 @@ Check if the gradient using finite differences matches the grad! provided.
 """
 function check_gradients(grad!, f, gradient, num_tests=10, tolerance=1.0e-5)
     for i in 1:num_tests
-        random_point = rand(length(gradient))
+        random_point = similar(gradient)
+        random_point .= rand(length(gradient))
         grad!(gradient, random_point)
         if norm(grad(central_fdm(5, 1), f, random_point)[1] - gradient) > tolerance
             @warn "There is a noticeable difference between the gradient provided and
