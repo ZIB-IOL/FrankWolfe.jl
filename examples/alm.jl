e5f4fee723f2975f3b3491d0d6919c87301c22ac
Jannis <hateadd@web.de>
2023-08-02 15:46:56 +0200

    Remove convex hull lmo

--- examples/alm.jl
+++ examples/alm.jl
@@ -23,14 +23,12 @@ lmoNB = FrankWolfe.ScaledBoundL1NormBall(-ones(n), ones(n))
 lmo_ball = FrankWolfe.KNormBallLMO(5, 1.0)
 lmo_sparse = FrankWolfe.KSparseLMO(100, 1.0)
 lmoProb = FrankWolfe.ProbabilitySimplexOracle(1.0)
-lmoConv = FrankWolfe.ConvHull(vertices)
 
 lmo_pairs = [
     (lmoProb, lmo_sparse),
     (lmoProb, lmo_ball),
     (lmoProb, lmoNB),
     (lmo_ball, lmoNB),
-    (lmo_ball, lmoConv)
 ]
 
 for pair in lmo_pairs
