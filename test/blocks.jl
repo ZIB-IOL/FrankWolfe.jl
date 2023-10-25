505b25c0042d7b242959a4b19dd94bff41a0c0e8
Mathieu Besançon <mathieu.besancon@gmail.com>
2023-10-25 14:49:13 +0200

    added iterate computation for sparse matrix (#442)
    * added iterate computation for sparse matrix
    
    * remove keyword argument
    
    * cleanup keyword argument
    
    * added test
    
    * undo change
    
    * Update print_callback_extension
    
    ---------
    
    Co-authored-by: Jannis <hateadd@web.de>

--- test/blocks.jl
+++ test/blocks.jl
@@ -47,44 +47,84 @@ using SparseArrays
 end
 
 @testset "FrankWolfe array methods" begin
-    mem = FrankWolfe.InplaceEmphasis()
-    arr0 = FrankWolfe.BlockVector([
-        [
-            1 3
-            4 6.0
-        ],
-        [
-            1 3 1
-            4 1 2
-            1 3 5.0
-        ],
-    ],)
-    arr1 = rand() * arr0
-    d = similar(arr1)
-    FrankWolfe.muladd_memory_mode(mem, d, arr1, arr0)
-    dref = arr1 - arr0
-    @test d == dref
-    gamma = rand()
-    arr2 = zero(arr0)
-    FrankWolfe.muladd_memory_mode(mem, arr2, gamma, arr0)
-    @test arr2 == -gamma * arr0
-    arr3 = similar(arr0)
-    FrankWolfe.muladd_memory_mode(mem, arr3, zero(arr0), gamma, arr0)
-    @test arr3 == -gamma * arr0
+    @testset "Block arrays" begin
+        mem = FrankWolfe.InplaceEmphasis()
+        arr0 = FrankWolfe.BlockVector([
+            [
+                1 3
+                4 6.0
+            ],
+            [
+                1 3 1
+                4 1 2
+                1 3 5.0
+            ],
+        ],)
+        arr1 = rand() * arr0
+        d = similar(arr1)
+        FrankWolfe.muladd_memory_mode(mem, d, arr1, arr0)
+        dref = arr1 - arr0
+        @test d == dref
+        gamma = rand()
+        arr2 = zero(arr0)
+        FrankWolfe.muladd_memory_mode(mem, arr2, gamma, arr0)
+        @test arr2 == -gamma * arr0
+        arr3 = similar(arr0)
+        FrankWolfe.muladd_memory_mode(mem, arr3, zero(arr0), gamma, arr0)
+        @test arr3 == -gamma * arr0
 
-    active_set = FrankWolfe.ActiveSet(
-        [0.25, 0.75],
-        [
+        active_set = FrankWolfe.ActiveSet(
+            [0.25, 0.75],
+            [
+                FrankWolfe.BlockVector([ones(100, 1), ones(30, 30)]),
+                FrankWolfe.BlockVector([3 * ones(100, 1), zeros(30, 30)]),
+            ],
             FrankWolfe.BlockVector([ones(100, 1), ones(30, 30)]),
-            FrankWolfe.BlockVector([3 * ones(100, 1), zeros(30, 30)]),
-        ],
-        FrankWolfe.BlockVector([ones(100, 1), ones(30, 30)]),
-    )
-    FrankWolfe.compute_active_set_iterate!(active_set)
-    x = active_set.x
-    x_copy = 0 * x
-    for (λi, ai) in active_set
-        @. x_copy += λi * ai
+        )
+        FrankWolfe.compute_active_set_iterate!(active_set)
+        x = active_set.x
+        x_copy = 0 * x
+        for (λi, ai) in active_set
+            @. x_copy += λi * ai
+        end
+        @test norm(x_copy - x) ≤ 1e-12
     end
-    @test norm(x_copy - x) ≤ 1e-12
+    @testset "Sparse vectors as atoms" begin
+        v1 = spzeros(3)
+        v2 = spzeros(3) .+ 1
+        v3 = spzeros(3)
+        v3[1] = 1
+        active_set = FrankWolfe.ActiveSet(
+            1/3 * ones(3),
+            [v1, v2, v3],
+            spzeros(3),
+        )
+        FrankWolfe.compute_active_set_iterate!(active_set)
+        active_set_dense = FrankWolfe.ActiveSet(
+            1/3 * ones(3),
+            collect.([v1, v2, v3]),
+            zeros(3),
+        )
+        FrankWolfe.compute_active_set_iterate!(active_set_dense)
+        @test active_set.x ≈ active_set_dense.x
+    end
+    @testset "Sparse matrices as atoms" begin
+        v1 = sparse(1.0I, 3, 3)
+        v2 = sparse(2.0I, 3, 3)
+        v2[1,2] = 2
+        v3 = spzeros(3, 3)
+        active_set = FrankWolfe.ActiveSet(
+            1/3 * ones(3),
+            [v1, v2, v3],
+            spzeros(3,3),
+        )
+        FrankWolfe.compute_active_set_iterate!(active_set)
+        active_set_dense = FrankWolfe.ActiveSet(
+            1/3 * ones(3),
+            collect.([v1, v2, v3]),
+            zeros(3,3),
+        )
+        FrankWolfe.compute_active_set_iterate!(active_set_dense)
+        @test active_set.x ≈ active_set_dense.x
+    end
 end
