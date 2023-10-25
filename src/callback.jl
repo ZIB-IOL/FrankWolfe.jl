9a7c4e04f15ce852932ae401c9c1c84cd6757aaa
Jannis <hateadd@web.de>
2023-10-25 10:54:57 +0200

    Update print_callback_extension

--- src/callback.jl
+++ src/callback.jl
@@ -22,22 +22,23 @@ Handles formating of the callback state into a table format with consistent leng
 function print_callback(data, format_string; print_header=false, print_footer=false)
     print_formatted(fmt, args...) = @eval @printf($fmt, $(args...))
 
-    lenHeaderFooter = compute_line_length(format_string)
+    if print_header || print_footer
+        lenHeaderFooter = compute_line_length(format_string)
+    end
 
     if print_footer
         line = "-"^lenHeaderFooter
-        @printf("%s\n", line)
+        @printf("%s\e[s\n", line) # \e[s stores the cursor position at the end of the line
     elseif print_header
         line = "-"^lenHeaderFooter
         @printf("\n%s\n", line)
         s_format_string = replace(format_string, "e" => "s")
         s_format_string = replace(s_format_string, "i" => "s")
         print_formatted(s_format_string, data...)
-        @printf("%s\n", line)
+        @printf("%s\e[s\n", line) # \e[s stores the cursor position at the end of the line    
     else
         print_formatted(format_string, data...)
     end
-    return lenHeaderFooter
 end
 
 """
@@ -48,15 +49,14 @@ Prints the state to the console after print_iter iterations.
 If the callback to be wrapped is of type nothing, always return true to enforce boolean output for non-nothing callbacks.
 """
 function make_print_callback(callback, print_iter, headers, format_string, format_state)
-    return function callback_with_prints(state, args...; kwargs...)
-        length_prev_msg = 0
+    return function callback_with_prints(state, args...)
         if (state.tt == pp || state.tt == last)
             if state.t == 0 && state.tt == last
                 print_callback(headers, format_string, print_header=true)
             end
             rep = format_state(state, args...)
             print_callback(rep, format_string)
-            length_prev_msg = print_callback(nothing, format_string, print_footer=true)
+            print_callback(nothing, format_string, print_footer=true)
             flush(stdout)
         elseif state.t == 1 ||
                mod(state.t, print_iter) == 0 ||
@@ -67,29 +67,28 @@ function make_print_callback(callback, print_iter, headers, format_string, forma
                 print_callback(headers, format_string, print_header=true)
             end
             rep = format_state(state, args...)
-            length_prev_msg = print_callback(rep, format_string)
+            extended_format_string = format_string[1:end-1] * "\e[s" * format_string[end] # Add escape code for storing cursor position before newline
+            print_callback(rep, extended_format_string)
             flush(stdout)
         end
 
         if callback === nothing
             return true
         end
-        return callback(state, args...; length_prev_msg=length_prev_msg, kwargs...)
+        return callback(state, args...)
     end
 end
 
 function make_print_callback_extension(callback, print_iter, headers, format_string, format_state)
-    return function callback_with_prints(state, args...; length_prev_msg=nothing, kwargs...)
-        tab() = print("\e[" * string(length_prev_msg) * "C")
+    return function callback_with_prints(state, args...)
         if (state.tt == pp || state.tt == last)
             if state.t == 0 && state.tt == last
                 print_callback(headers, format_string, print_header=true)
             end
             rep = format_state(state, args...)
-            print("\e[F\e[F")
-            tab()
+            print("\e[u\e[1A\e[2D") # Move to end of "last"-row
             print_callback(rep, format_string)
-            tab()
+            print("\e[u\e[2D") # Move to end of horizontal line
             print_callback(nothing, format_string, print_footer=true)
             flush(stdout)
         elseif state.t == 1 ||
@@ -97,19 +96,18 @@ function make_print_callback_extension(callback, print_iter, headers, format_str
                state.tt == dualstep ||
                state.tt == last
             if state.t == 1
-                print("\e[F\e[F\e[F\e[F")
-                tab()
+                print("\e[u\e[3A") # Move to end of upper horizontal line
                 line = "-"^compute_line_length(format_string)
                 @printf("%s\n", line)
-                tab()
+                print("\e[u\e[2A") # Move to end of the header row
                 s_format_string = replace(format_string, "e" => "s")
                 s_format_string = replace(s_format_string, "i" => "s")
                 @eval @printf($s_format_string, $(headers...))
-                tab()
+                print("\e[u\e[1A") # Move to end of lower horizontal line
                 @printf("%s\n\n", line)
             end
             rep = format_state(state, args...)
-            print("\e[F\e[" * string(length_prev_msg) * "C")
+            print("\e[u") # Move to end of table row
             print_callback(rep, format_string)
             flush(stdout)
         end
@@ -117,7 +115,7 @@ function make_print_callback_extension(callback, print_iter, headers, format_str
         if callback === nothing
             return true
         end
-        return callback(state, args...; kwargs...)
+        return callback(state, args...)
     end
 end
 
@@ -132,13 +130,13 @@ The state data is only the 5 first fields, usually:
 If the callback to be wrapped is of type nothing, always return true to enforce boolean output for non-nothing callbacks.
 """
 function make_trajectory_callback(callback, traj_data::Vector)
-    return function callback_with_trajectory(state, args...; kwargs...)
+    return function callback_with_trajectory(state, args...)
         if state.tt !== last || state.tt !== pp
             push!(traj_data, callback_state(state))
         end
         if callback === nothing
             return true
         end
-        return callback(state, args...; kwargs...)
+        return callback(state, args...)
     end
 end
