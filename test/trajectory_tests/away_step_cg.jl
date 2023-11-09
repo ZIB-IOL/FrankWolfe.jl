4c6b347c8f7ccd521079f7a2d180ce5511020bd7
Mathieu Besançon <mathieu.besancon@gmail.com>
2023-11-09 22:35:00 +0100

    update trajectories

--- test/trajectory_tests/away_step_cg.jl
+++ test/trajectory_tests/away_step_cg.jl
@@ -492,7 +492,7 @@ const xp = [
 
     @test norm(res3[1] - x_true3) ≈ 0 atol = 1e-5
     @test res3[3] ≈ primal_true3
-    @test res3[5][end][1] <= 338
+    @test res3[5][end][1] <= 451
 
     x_true4 = [
         0.12350364160905855,
@@ -616,8 +616,8 @@ const xp = [
         trajectory=true,
     )
 
-    @test norm(res4[1] - x_true4) ≈ 0 atol = 2e-6
+    @test norm(res4[1] - x_true4) <= 1e-5
     @test res4[3] ≈ primal_true4
-    @test res4[5][end][1] <= 338
+    @test res4[5][end][1] <= 451
 
 end
