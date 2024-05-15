cbf579a79d860cdf2ba1f54dbd15bbe20021e0d4
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-05-15 18:38:21 +0200

    Update simplex_oracles.jl

--- src/simplex_oracles.jl
+++ src/simplex_oracles.jl
@@ -193,7 +193,8 @@ function convert_mathopt(
     MOI.empty!(optimizer)
     τ = lmo.right_side
     n = dimension
-    (x, _) = MOI.add_constrained_variables(optimizer, [MOI.Interval(0.0, 1.0) for _ in 1:n])
+    (x, _) = MOI.add_constrained_variables(optimizer, [MOI.Interval(0.0, τ) for _ in 1:n])
+    #(x, _) = MOI.add_constrained_variables(optimizer, [MOI.Interval(0.0, 1.0) for _ in 1:n])
     MOI.add_constraint(
         optimizer,
         MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(ones(n), x), 0.0),
