001d040bfe6fa3c2a5d427855792a0ec752e895c
Sébastien Designolle <sebastien.designolle@gmail.com>
2024-06-24 17:00:23 +0200

    Further syntax polishing

--- examples/birkhoff_polytope.jl
+++ examples/birkhoff_polytope.jl
@@ -5,19 +5,17 @@ import GLPK
 
 include("../examples/plot_utils.jl")
 
-s = rand(1:100)
+# s = rand(1:100)
 s = 98
 @info "Seed $s"
 Random.seed!(s)
 
-
 n = Int(1e2)
 k = 3000
 
-xpi = rand(n * n);
-total = sum(xpi);
-xpi = reshape(xpi, n, n)
-const xp = xpi # ./ total;
+xpi = rand(n, n)
+# total = sum(xpi)
+const xp = xpi # / total
 const normxp2 = dot(xp, xp)
 
 # better for memory consumption as we do coordinate-wise ops
@@ -30,9 +28,6 @@ function cgrad!(storage, x, xp)
     return @. storage = 2 * (x - xp) / n^2
 end
 
-# initial direction for first vertex
-direction_mat = randn(n, n)
-
 # BirkhoffPolytopeLMO via Hungarian Method
 lmo_native = FrankWolfe.BirkhoffPolytopeLMO()
 
@@ -42,7 +37,9 @@ lmo_moi = FrankWolfe.convert_mathopt(lmo_native, GLPK.Optimizer(), dimension=n)
 # choose between lmo_native (= Hungarian Method) and lmo_moi (= LP formulation solved with GLPK)
 lmo = lmo_native
 
-x00 = FrankWolfe.compute_extreme_point(lmo, direction_mat)
+# initial direction for first vertex
+direction_mat = randn(n, n)
+x0 = FrankWolfe.compute_extreme_point(lmo, direction_mat)
 
 FrankWolfe.benchmark_oracles(
     x -> cf(x, xp, normxp2),
@@ -53,14 +50,11 @@ FrankWolfe.benchmark_oracles(
 )
 
 # BCG run
-
-x0 = deepcopy(x00)
-
 @time x, v, primal, dual_gap, trajectoryBCG, _ = FrankWolfe.blended_conditional_gradient(
     x -> cf(x, xp, normxp2),
     (str, x) -> cgrad!(str, x, xp),
     lmo,
-    x0,
+    x0;
     max_iteration=k,
     line_search=FrankWolfe.Shortstep(2/n^2),
     print_iter=k / 10,
