5b1ee1088efc4ab64bc0ade05f12e07a400bff16
Sébastien Designolle <sebastien.designolle@gmail.com>
2024-06-24 19:57:37 +0200

    Add second mirror symmetry, 62s vs 16s

--- examples/birkhoff_polytope_symmetric.jl
+++ examples/birkhoff_polytope_symmetric.jl
@@ -3,37 +3,57 @@ using LinearAlgebra
 using Random
 import GLPK
 
-function build_reduce_inflate_permutedims(p::Array{T, 2}) where {T <: Number}
+function build_reduce_inflate(p::Array{T, 2}) where {T <: Number}
     n = size(p, 1)
     @assert n == size(p, 2)
-    dimension = (n * (n + 1)) ÷ 2
+    dimension = floor(Int, (n+1)^2 / 4)
     sqrt2 = sqrt(T(2))
     return function(A::AbstractArray{T, 2}, lmo)
         vec = Vector{T}(undef, dimension)
         cnt = 0
-        @inbounds for i in 1:n
-            vec[i] = A[i, i]
-            cnt += n - i
-            for j in i+1:n
-                vec[cnt+j] = (A[i, j] + A[j, i]) / sqrt2
+        @inbounds for i in 1:(n+1)÷2, j in i:n+1-i
+            cnt += 1
+            if i == j
+                if i + j == n+1
+                    vec[cnt] = A[i, i]
+                else
+                    vec[cnt] = (A[i, i] + A[n+1-i, n+1-i]) / sqrt2
+                end
+            else
+                if i + j == n+1
+                    vec[cnt] = (A[i, j] + A[j, i]) / sqrt2
+                else
+                    vec[cnt] = (A[i, j] + A[j, i] + A[n+1-i, n+1-j] + A[n+1-j, n+1-i]) / 2
+                end
             end
         end
         return FrankWolfe.SymmetricArray(collect(A), vec)
     end, function(x::FrankWolfe.SymmetricArray, lmo)
         cnt = 0
-        @inbounds for i in 1:n
-            x.data[i, i] = x.vec[i]
-            cnt += n - i
-            for j in i+1:n
-                x.data[i, j] = x.vec[cnt+j] / sqrt2
-                x.data[j, i] = x.data[i, j]
+        @inbounds for i in 1:(n+1)÷2, j in i:n+1-i
+            cnt += 1
+            if i == j
+                if i + j == n+1
+                    x.data[i, i] = x.vec[cnt]
+                else
+                    x.data[i, i] = x.vec[cnt] / sqrt2
+                    x.data[n+1-i, n+1-i] = x.data[i, j]
+                end
+            else
+                if i + j == n+1
+                    x.data[i, j] = x.vec[cnt] / sqrt2
+                    x.data[j, i] = x.data[i, j]
+                else
+                    x.data[i, j] = x.vec[cnt] / 2
+                    x.data[j, i] = x.data[i, j]
+                    x.data[n+1-i, n+1-j] = x.data[i, j]
+                    x.data[n+1-j, n+1-i] = x.data[i, j]
+                end
             end
         end
         return x.data
     end
 end
-
-include("../examples/plot_utils.jl")
 
 # s = rand(1:100)
 s = 98
@@ -43,14 +63,14 @@ Random.seed!(s)
 n = Int(2e2)
 k = Int(4e4)
 
+# we artificially create a symmetric instance to illustrate the syntax
 xpi = rand(n, n)
 xpi .+= xpi'
-# total = sum(xpi)
-reduce, inflate = build_reduce_inflate_permutedims(xpi)
-const xp = reduce(xpi, nothing) # / total
+xpi .+= reverse(xpi)
+xpi ./= 4
+reduce, inflate = build_reduce_inflate(xpi)
+const xp = xpi
 const normxp2 = dot(xp, xp)
-
-# better for memory consumption as we do coordinate-wise ops
 
 function cf(x, xp, normxp2)
     return (normxp2 - 2dot(x, xp) + dot(x, x)) / n^2
@@ -60,37 +80,39 @@ function cgrad!(storage, x, xp)
     return @. storage = 2 * (x - xp) / n^2
 end
 
-# choose between lmo_native (= Hungarian Method) and lmo_moi (= LP formulation solved with GLPK)
-lmo = FrankWolfe.SymmetricLMO(FrankWolfe.BirkhoffPolytopeLMO(), reduce, inflate)
+lmo_nat = FrankWolfe.BirkhoffPolytopeLMO()
 
-# initial direction for first vertex
-direction_mat = reduce(randn(n, n), nothing)
-x0 = FrankWolfe.compute_extreme_point(lmo, direction_mat)
+x0 = FrankWolfe.compute_extreme_point(lmo_nat, randn(n, n))
 
-FrankWolfe.benchmark_oracles(
-    x -> cf(x, xp, normxp2),
-    (str, x) -> cgrad!(str, x, xp),
-    () -> reduce(randn(n, n), nothing),
-    lmo;
-    k=100,
-)
-
-# BPCG run
 @time x, v, primal, dual_gap, trajectoryBPCG, _ = FrankWolfe.blended_pairwise_conditional_gradient(
     x -> cf(x, xp, normxp2),
     (str, x) -> cgrad!(str, x, xp),
-    lmo,
+    lmo_nat,
     FrankWolfe.ActiveSetQuadratic([(1.0, x0)], 2I/n^2, -2xp/n^2);
     max_iteration=k,
     line_search=FrankWolfe.Shortstep(2/n^2),
     lazy=true,
     print_iter=k / 10,
-    memory_mode=FrankWolfe.InplaceEmphasis(),
-    trajectory=true,
     verbose=true,
 )
 
-data = [trajectoryBPCG]
-label = ["BPCG"]
+const rxp = reduce(xpi, nothing)
+@assert dot(rxp, rxp) ≈ normxp2
 
-plot_trajectories(data, label, reduce_size=true, marker_shapes=[:dtriangle])
+lmo_sym = FrankWolfe.SymmetricLMO(lmo_nat, reduce, inflate)
+
+rx0 = FrankWolfe.compute_extreme_point(lmo_sym, reduce(randn(n, n), nothing))
+
+@time x, v, primal, dual_gap, trajectoryBPCG, _ = FrankWolfe.blended_pairwise_conditional_gradient(
+    x -> cf(x, rxp, normxp2),
+    (str, x) -> cgrad!(str, x, rxp),
+    lmo_sym,
+    FrankWolfe.ActiveSetQuadratic([(1.0, rx0)], 2I/n^2, -2rxp/n^2);
+    max_iteration=k,
+    line_search=FrankWolfe.Shortstep(2/n^2),
+    lazy=true,
+    print_iter=k / 10,
+    verbose=true,
+)
+
+println()
