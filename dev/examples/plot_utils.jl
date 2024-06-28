346970a6fce2612fe9002df7084dd71a03bf37f3
Documenter.jl <documenter@juliadocs.github.io>
2024-06-28 12:24:46 +0000

    build based on 9be046c

--- dev/examples/plot_utils.jl
+++ dev/examples/plot_utils.jl
@@ -1,5 +1,4 @@
 using Plots
-using FiniteDifferences
 
 """
 plot_results
@@ -420,19 +419,4 @@ function plot_sparsity(
         savefig(fp, filename)
     end
     return fp
-end
-
-"""
-Check if the gradient using finite differences matches the grad! provided.
-"""
-function check_gradients(grad!, f, gradient, num_tests=10, tolerance=1.0e-5)
-    for i in 1:num_tests
-        random_point = similar(gradient)
-        random_point .= rand(length(gradient))
-        grad!(gradient, random_point)
-        if norm(grad(central_fdm(5, 1), f, random_point)[1] - gradient) > tolerance
-            @warn "There is a noticeable difference between the gradient provided and
-            the gradient computed using finite differences.:\n$(norm(grad(central_fdm(5, 1), f, random_point)[1] - gradient))"
-        end
-    end
 end
