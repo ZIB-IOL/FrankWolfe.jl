906ef904e6c67d51214ad0a83368aba77df5cba2
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-08-07 16:40:21 +0200

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -384,3 +384,30 @@ function weight_purge_threshold_default(::Type{T}) where {T<:AbstractFloat}
 end
 weight_purge_threshold_default(::Type{T}) where {T<:Number} = Base.rtoldefault(T)
 
+"""
+    Computes the linear minimizer in the direction on the PrecomputedSet.
+"""
+function pre_computed_set_argminmax(pre_computed_set, direction)
+    val = Inf
+    valM = -Inf
+    idx = -1
+    idxM = -1
+    for i in eachindex(pre_computed_set)
+        temp_val = fast_dot(pre_computed_set[i], direction)
+        if temp_val < val
+            val = temp_val
+            idx = i
+        end
+        if valM < temp_val
+            valM = temp_val
+            idxM = i
+        end
+    end
+    if idx == -1 || idxM == -1
+        error("Infinite minimum $val or maximum $valM in the PrecomputedSet. Does the gradient contain invalid (NaN / Inf) entries?")
+    end
+    v_local = pre_computed_set[idx]
+    a_local = pre_computed_set[idxM]
+    return (v_local, idx, val, a_local, idxM, valM)
+end
+
