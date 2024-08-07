07a3c8b6446217ab37dfc1cd354b74dc5b243ffe
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-08-07 16:38:25 +0200

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -383,3 +383,4 @@ function weight_purge_threshold_default(::Type{T}) where {T<:AbstractFloat}
     return sqrt(eps(T) * Base.rtoldefault(T)) # around 1e-12 for Float64
 end
 weight_purge_threshold_default(::Type{T}) where {T<:Number} = Base.rtoldefault(T)
+
