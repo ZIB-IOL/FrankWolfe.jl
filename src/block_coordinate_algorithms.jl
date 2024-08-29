bb4b795c4ff158162a08ec40023cf4cbd949fb22
Zev Woodstock <woodstock@zib.de>
2024-08-29 08:52:27 -0400

    Added Lazy Updates from Block-CG project

--- src/block_coordinate_algorithms.jl
+++ src/block_coordinate_algorithms.jl
@@ -56,6 +56,34 @@ end
 DualProgressOrder() = DualProgressOrder(-1, [], [])
 DualProgressOrder(i::Int64) = DualProgressOrder(i, [], [])
 
+"""
+The Lazy update order is discussed in "Flexible block-iterative
+analysis for the Frank-Wolfe algorithm," by Braun, Pokutta, &
+Woodstock (2024). 
+'lazy_block' is an index of a computationally expensive block to
+update;
+'refresh_rate' describes the frequency at which we perform
+a full activation; and
+'block_size' describes the number of "faster" blocks
+(i.e., those excluding 'lazy_block') activated (chosen
+uniformly at random) during each
+of the "faster" iterations; for more detail, see the
+article. If  'block_size' is unspecified, this defaults to
+1. 
+Note: This methodology is currently only proven to work
+with 'FrankWolfe.Shortstep' linesearches and a (not-yet
+implemented) adaptive method; see the article for details.
+"""
+struct LazyUpdate <: BlockCoordinateUpdateOrder 
+    lazy_block::Int
+    refresh_rate::Int
+    block_size::Int
+end
+
+function LazyUpdate(lazy_block::Int,refresh_rate::Int)
+    return LazyUpdate(lazy_block, refresh_rate, 1)
+end
+
 function select_update_indices(::FullUpdate, s::CallbackState, _)
     return [1:length(s.lmo.lmos)]
 end
@@ -133,6 +161,15 @@ function select_update_indices(u::DualGapOrder, s::CallbackState, dual_gaps)
 
     n = u.limit == -1 ? l : u.limit
     return [[findfirst(cumsum(dual_gaps/sum(dual_gaps)) .> rand())] for _=1:n]
+end
+
+function select_update_indices(update::LazyUpdate, s::CallbackState, dual_gaps)
+    #Returns a sequence of randomized cheap indices by 
+    #excluding update.lazy_block until "refresh_rate" updates
+    #occur, then adds an update of everything while mainting
+    #randomized order.
+    l = length(s.lmo.lmos)
+    return push!([[rand(range(1,l)[1:l .!= update.lazy_block]) for _ in range(1,update.block_size)] for _ in 1:(update.refresh_rate -1)], range(1,l))
 end
 
 """
