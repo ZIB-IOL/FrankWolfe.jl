b4ba4ad9d11715a062b847321fd3a12b1b6399f2
Mathieu Besançon <mathieu.besancon@gmail.com>
2024-12-13 12:53:02 +0100

    add safety check for adaptive (#534)
    * add safety check for adaptive
    
    * SCIP in test deps
    
    * Revert "SCIP in test deps"
    
    This reverts commit 8c287e222b55f94ce2fe71e962b759c6a000b2b7.
    
    * SCIP to HiGHS
    
    * Added failing instance as a test.
    
    ---------
    
    Co-authored-by: Hendrych <dhendryc@mob-009366.zib.de>

--- examples/optimal_experiment_design.jl
+++ examples/optimal_experiment_design.jl
@@ -4,11 +4,12 @@ using Random
 using Distributions
 using LinearAlgebra
 using Statistics
-using SCIP
+using HiGHS
 using MathOptInterface
 const MOI = MathOptInterface
 using SparseArrays
 using Test
+using StableRNGs
 
 # The Optimal Experiment Design Problem consists of choosing a subset of experiments
 # maximising the information gain.
@@ -34,7 +35,10 @@ seed - for the Random functions.
 m    - number of experiments.
 Build the experiment matrix A.
 """
-function build_data(m)
+function build_data(m; seed=nothing)
+    if seed != nothing
+        rng = StableRNG(seed)
+    end
     n = Int(floor(m/10))
     B = rand(m,n)
     B = B'*B
@@ -51,7 +55,7 @@ end
 Build MOI version of the lmo.
 """
 function build_moi_lmo(m)
-    o = SCIP.Optimizer()
+    o = HiGHS.Optimizer()
     MOI.empty!(o)
     MOI.set(o, MOI.Silent(), true)
 
@@ -273,7 +277,7 @@ m = 300
         x_b, _, primal, dual_gap, traj_data_b, _ = FrankWolfe.blended_conditional_gradient(f, grad!, lmo, x0, verbose=true, trajectory=true,line_search=FrankWolfe.Secant(domain_oracle=domain_oracle))
 
         @test traj_data_s[end][1] < traj_data[end][1]
-        @test traj_data_d[end][1] <= traj_data_s[end][1]
+        @test traj_data_d[end][1] <= traj_data[end][1]
         @test traj_data_b[end][1] <= traj_data_s[end][1]
         @test isapprox(f(x_s), f(x))
         @test isapprox(f(x_s), f(x_d))
@@ -314,11 +318,24 @@ m = 300
         x_b, _, primal, dual_gap, traj_data_b, _ = FrankWolfe.blended_conditional_gradient(f, grad!, lmo, x0, verbose=true, trajectory=true,line_search=FrankWolfe.Secant(domain_oracle=domain_oracle))
 
         @test traj_data_s[end][1] < traj_data[end][1]
-        @test traj_data_d[end][1] <= traj_data_s[end][1]
+        @test traj_data_d[end][1] <= traj_data[end][1]
         @test traj_data_b[end][1] <= traj_data_s[end][1]
         @test isapprox(f(x_s), f(x))
         @test isapprox(f(x_s), f(x_d))
         @test isapprox(f(x_s), f(x_b))
     end
+end
+
+@testset "Failing Optimal Design Instance with AFW" begin 
+    seed = 8775360557774874450
+    m = 100
+    A = build_data(m, seed=seed)
+    f, grad! = build_a_criterion(A)
+
+    lmo = FrankWolfe.ProbabilitySimplexOracle(1.0)
+    x0, active_set, _ = build_start_point(A)
+
+    _, _, primal, dual_gap, _, _ = FrankWolfe.away_frank_wolfe(f, grad!, lmo, active_set; verbose=true) 
+    @test isfinite(primal)
 end
 
