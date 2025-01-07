17e8ecfcf876f8fc1ce84b38fd23c4ec5a5b26a2
Hendrych <dhendryc@mob-009366.zib.de>
2025-01-07 18:39:51 +0100

    Set up the job schedule.

---
+++ experiments/run_jobs.jl
@@ -0,0 +1,48 @@
+using Random
+using FrankWolfe
+
+include("solve_problems.jl")
+
+"""
+DON'T FORGET TO ADD e AGAIN ONCE YOU ARE DONE DEBUGGING!!
+"""
+problem = ENV["PROBLEM"]
+seed = parse(Int, ENV["SEED"])
+m = parse(Int, ENV["DIMENSION"])
+
+time_limit = 3600 # one hour time limit
+seeds = seed == 0 ? [1,2,3,4,5] : [seed]
+
+ls = ENV["LINESEARCH"]
+linesearches = ["Secant", "SecantBT", "Adaptive"] #"BacktrackingSecant 
+line_search = if ls == "Secant"
+    LS_ONLY_SECANT
+elseif ls == "SecantBT"
+    LS_SECANT_WITH_BACKTRACKING
+elseif ls == "Adaptive"
+    LS_ADAPTIVE
+elseif ls == "BacktrackingSecant"
+    LS_BACKTRACKING_AND_SECANT
+else
+    error("Unknown linesearch")
+end
+
+# For the vector.valued problems, square the input dimension
+if problem in ["OEDP_A", "OEDP_D", "QuadraticProbSimplex"]
+    m = m^2
+end
+
+@show problem, string(line_search), m, seed
+
+for seed in seeds
+    @show seed
+    try
+        solve_problems(seed, m, problem, line_search)
+    catch e
+        showerror(stdout, e, catch_backtrace())
+        error_file = problem * "_" * string(line_search) * ".txt" 
+        open(error_file,"a") do io
+            println(io, seed, " ", m, " : ", e)
+        end
+    end
+end
---
+++ experiments/schedule_jobs.jl
@@ -0,0 +1,15 @@
+problems = ["OEDP_A", "OEDP_D", "Nuclear", "Birkhoff", "QuadraticProbSimplex", "Spectrahedron"] 
+dimensions = collect(100:100:1000) # square root of the dimension
+seeds = [0] # [1,2,3,4,5]
+linesearches = ["Secant", "SecantBT", "Adaptive"] #"BacktrackingSecant 
+
+for line_search in linesearches
+    for problem in problems
+        for dim in dimensions
+            for seed in seeds
+                @show line_search, problem, dim, seed
+                run(`sbatch -A optimi -J Secant jobs.sbatch $line_search $problem $dim $seed`) # CB
+            end
+        end
+    end
+end
