db6851ba196bf255f58cf01248337cb7bf86606d
Hendrych <dhendryc@mob-009366.zib.de>
2025-01-08 15:34:33 +0100

    Add new example and linesearch. Minor fixes.

--- experiments/solve_problems.jl
+++ experiments/solve_problems.jl
@@ -2,9 +2,10 @@ using FrankWolfe
 using LinearAlgebra
 using DataFrames
 using CSV
+using Random
 
 
-for file in readdir(joinpath(@__DIR__, "/problems/"), join=true)
+for file in readdir(joinpath(@__DIR__, "problems/"), join=true)
     if endswith(file, "jl")
         include(file)
     end
@@ -26,9 +27,9 @@ const linesearchvariant_string = (
 
 function solve_problems(seed, dimension, problem, ls_variant; time_limit=3600, write=true, verbose=true, FW_variant="BPCG")
     f, grad!, lmo, x0, active_set, domain_oracle = if problem == "OEDP_A"
-        build_a_criterion(seed, dimension^2, criterion="A")
+        build_optimal_criterion(seed, dimension^2, criterion="A")
     elseif problem == "OEDP_D"
-        build_a_criterion(seed, dimension^2, criterion="D")
+        build_optimal_criterion(seed, dimension^2, criterion="D")
     elseif problem == "Nuclear"
         build_nuclear_norm_problem(seed, dimension)
     elseif problem == "Birkhoff"
@@ -37,6 +38,8 @@ function solve_problems(seed, dimension, problem, ls_variant; time_limit=3600, w
         build_simple_self_concordant_problem(seed, dimension^2)
     elseif problem == "Spectrahedron"
         build_spectrahedron(seed, dimension)
+    elseif problem == "IllConditionedQuadratic"
+        build_ill_conditioned_quadratic(seed, dimension^2)
     else
         error("Problem type not known.")
     end
@@ -51,25 +54,28 @@ function solve_problems(seed, dimension, problem, ls_variant; time_limit=3600, w
     line_search = if ls_variant == LS_ONLY_SECANT
         FrankWolfe.Secant(safe=false, domain_oracle=domain_oracle)
     elseif ls == LS_BACKTRACKING_AND_SECANT
+        FrankWolfe.BacktrackingAndSecant(domain_oracle=domain_oracle)
     elseif ls == LS_SECANT_WITH_BACKTRACKING
         FrankWolfe.Secant(safe=true, domain_oracle=domain_oracle)
     elseif ls == LS_ADAPTIVE
         FrankWolfe.Adaptive(domain_oracle=domain_oracle)
     end
     # Precompile run
-    fw_variant(f, grad!, lmo, active_set, line_search=line_search, time_limit=10, max_iteration=Inf)
+    fw_variant(f, grad!, lmo, active_set, line_search=line_search, timeout=10, max_iteration=Inf)
 
     # Actual run
-    data = @timed fw_variant(f, grad!, lmo, active_set, line_search=line_search, time_limit=time_limit, max_iteration=Inf, verbose=verbose, trajectory=true)
+    data = @timed fw_variant(f, grad!, lmo, active_set, line_search=line_search, timeout=time_limit, max_iteration=Inf, verbose=verbose, trajectory=true)
 
     @show data.value.primal, data.value.dual_gap
     @show data.value.traj_data[end][1], data.value.traj_data[end][end]
-    @show data.value.x 
-    @show data.value.v
-    @show data.value.active_set
+    #@show data.value.x 
+    #@show data.value.v
+    #@show data.value.active_set
 
     df = DataFrame(seed=seed, dimension=dimension^2, time=data.time, fw_time=data.value.traj_data[end][end], primal = data.value.primal, dual_gap=data.value.dual_gap, iterations=data.value.traj_data[end][1])
     
     file_name = joinpath(@__DIR__, "csv/" * problem * "/" * string(ls_variant) * string(dimension^2) * "_" * string(seed) * ".csv")
     CSV.write(file_name, df, append=false, writeheader=true)
+
+    return data.value.x, data.value.primal, data.value.dual_gap
 end
