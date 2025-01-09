c75ebace183fb87d73c9ef8fa4de35fd3542d970
Deborah Hendrych <d.hendrych@web.de>
2025-01-09 21:57:07 +0100

    Added Adaptive + Secant and Adaptive Zero + Secant.

--- experiments/solve_problems.jl
+++ experiments/solve_problems.jl
@@ -16,6 +16,8 @@ end
     LS_SECANT_WITH_BACKTRACKING = 2
     LS_BACKTRACKING_AND_SECANT = 3
     LS_ADAPTIVE = 4
+    LS_ADAPTIVE_AND_SECANT = 5
+    LS_ADAPTIVE_ZERO_AND_SECANT = 6
 end
 
 const linesearchvariant_string = (
@@ -23,6 +25,8 @@ const linesearchvariant_string = (
     LS_SECANT_WITH_BACKTRACKING="Secant_with_Backtracking",
     LS_BACKTRACKING_AND_SECANT="Backtracking_and_Secant",
     LS_ADAPTIVE="Adaptive",
+    LS_ADAPTIVE_AND_SECANT="Adaptive_and_Secant",
+    LS_ADAPTIVE_ZERO_AND_SECANT="Adaptive_Zero_and_Secant",
 )
 
 function solve_problems(seed, dimension, problem, ls_variant; time_limit=3600, write=true, verbose=true, FW_variant="BPCG", max_iter=Inf)
@@ -54,28 +58,36 @@ function solve_problems(seed, dimension, problem, ls_variant; time_limit=3600, w
     line_search = if ls_variant == LS_ONLY_SECANT
         FrankWolfe.Secant(safe=false, domain_oracle=domain_oracle)
     elseif ls_variant == LS_BACKTRACKING_AND_SECANT
-        FrankWolfe.BacktrackingAndSecant(domain_oracle=domain_oracle)
+        FrankWolfe.ImprovedGammaSecant(first_ls=FrankWolfe.Backtracking(),domain_oracle=domain_oracle)
     elseif ls_variant == LS_SECANT_WITH_BACKTRACKING
         FrankWolfe.Secant(safe=true, domain_oracle=domain_oracle)
     elseif ls_variant == LS_ADAPTIVE
         FrankWolfe.Adaptive(domain_oracle=domain_oracle)
+    elseif ls_variant == LS_ADAPTIVE_AND_SECANT
+        FrankWolfe.ImprovedGammaSecant(first_ls=FrankWolfe.Adaptive(), domain_oracle=domain_oracle)
+    elseif ls_variant == LS_ADAPTIVE_ZERO_AND_SECANT
+        FrankWolfe.ImprovedGammaSecant(first_ls=FrankWolfe.AdaptiveZerothOrder(), domain_oracle=domain_oracle)
+    else
+        error("Line search variant not known.")
     end
     # Precompile run
     fw_variant(f, grad!, lmo, active_set, line_search=line_search, timeout=10, max_iteration=max_iter)
 
     # Actual run
     data = @timed fw_variant(f, grad!, lmo, active_set, line_search=line_search, timeout=time_limit, max_iteration=max_iter, verbose=verbose, trajectory=true)
+    smallest_dual_gap = data.value.traj_data[end-2][end-1]
 
     @show data.value.primal, data.value.dual_gap
     @show data.value.traj_data[end][1], data.value.traj_data[end][end]
+    @show smallest_dual_gap
     #@show data.value.x 
     #@show data.value.v
     #@show data.value.active_set
 
     if write
-        df = DataFrame(seed=seed, dimension=dimension^2, time=data.time, fw_time=data.value.traj_data[end][end], primal = data.value.primal, dual_gap=data.value.dual_gap, iterations=data.value.traj_data[end][1])
+        df = DataFrame(seed=seed, dimension=dimension^2, time=data.time, fw_time=data.value.traj_data[end][end], primal = data.value.primal, dual_gap=data.value.dual_gap, smallest_dual_gap=smallest_dual_gap, iterations=data.value.traj_data[end][1])
         
-        file_name = joinpath(@__DIR__, "csv/" * problem * "/" * string(ls_variant) * string(dimension^2) * "_" * string(seed) * ".csv")
+        file_name = joinpath(@__DIR__, "csv/" * problem * "/" * string(ls_variant) * "_" * string(dimension^2) * "_" * string(seed) * ".csv")
         CSV.write(file_name, df, append=false, writeheader=true)
     end
 
