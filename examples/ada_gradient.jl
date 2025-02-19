0e3cc7808eb195b86c7cb50cb1dd4eedf936d203
Sebastian Pokutta <23001135+pokutta@users.noreply.github.com>
2025-02-19 12:18:43 +0100

    updated example!

--- examples/ada_gradient.jl
+++ examples/ada_gradient.jl
@@ -2,9 +2,9 @@ using FrankWolfe
 using LinearAlgebra
 using Random
 
-max_iter = Int(1e9)
+max_iter = Int(1e5)
 print_iter = max_iter // 100
-epsilon = 0 # 1e-10
+epsilon = 1e-10
 
 """
 Simple quadratic function f(x) = 1/2 * x'Qx + b'x
@@ -49,7 +49,7 @@ x1, f1, hist1 = FrankWolfe.adaptive_gradient_descent(
     x0;
     step0 = 0.1,
     max_iterations = max_iter,
-    print_iter = 100 + 0 * print_iter,
+    print_iter = print_iter,
     epsilon = epsilon,
     verbose = true
 )
@@ -65,7 +65,7 @@ x2, f2, hist2 = FrankWolfe.adaptive_gradient_descent2(
     grad!,
     x0;
     step0 = 0.1,
-    max_iterations = 100 + 0 * max_iter,
+    max_iterations = max_iter,
     print_iter = print_iter,
     epsilon = epsilon,
     verbose = true
