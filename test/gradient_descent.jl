8378b2e30a2e0ddca1b7f61f644f48d2866344f2
Mathieu Besançon <mathieu.besancon@gmail.com>
2025-02-25 16:54:08 +0100

    rename functions in gradient file to avoid overwritting

--- test/gradient_descent.jl
+++ test/gradient_descent.jl
@@ -28,9 +28,9 @@ end
 const hessian = transpose(matrix) * matrix
 const linear = rand(n)
 
-f(x) = dot(linear, x) + 0.5 * dot(x, hessian, x)
+f_gd(x) = dot(linear, x) + 0.5 * dot(x, hessian, x)
 
-function grad!(storage, x)
+function grad_gd!(storage, x)
     return storage .= linear + hessian * x
 end
 
@@ -38,7 +38,7 @@ const L = eigmax(hessian)
 
 # Compute optimal solution using direct solve for testing
 const x_opt = -hessian \ linear
-const f_opt = f(x_opt)
+const f_opt = g_df(x_opt)
 
 @testset "Adaptive Gradient Descent" begin
     @testset "Type $T" for T in (Float64, Double64)
@@ -47,8 +47,8 @@ const f_opt = f(x_opt)
 
         # Test first variant
         x1, f1, hist1 = FrankWolfe.adaptive_gradient_descent(
-            f,
-            grad!,
+            f_gd,
+            grad_gd!,
             convert.(T, x0);
             step0=convert(T, 0.1),
             max_iteration=k,
@@ -60,8 +60,8 @@ const f_opt = f(x_opt)
 
         # Test second variant
         x2, f2, hist2 = FrankWolfe.adaptive_gradient_descent2(
-            f,
-            grad!,
+            f_gd,
+            grad_gd!,
             convert.(T, x0);
             step0=convert(T, 0.1),
             max_iteration=k,
@@ -73,9 +73,9 @@ const f_opt = f(x_opt)
 
         # Test convergence to optimal solution
         g0 = similar(x1)
-        grad!(g0, x1)
+        grad_gd!(g0, x1)
         @test norm(g0) ≤ 2 * target_tolerance
-        grad!(g0, x2)
+        grad_gd!(g0, x2)
         @test norm(g0) ≤ 2 * target_tolerance
 
         # Test objective values
@@ -93,8 +93,8 @@ const f_opt = f(x_opt)
 
         # Test with InplaceEmphasis
         x_inplace, f_inplace, _ = FrankWolfe.adaptive_gradient_descent(
-            f,
-            grad!,
+            f_gd,
+            grad_gd!,
             x0;
             epsilon=target_tolerance,
             memory_mode=FrankWolfe.InplaceEmphasis(),
@@ -104,8 +104,8 @@ const f_opt = f(x_opt)
 
         # Test with OutplaceEmphasis
         x_outplace, f_outplace, _ = FrankWolfe.adaptive_gradient_descent(
-            f,
-            grad!,
+            f_gd,
+            grad_gd!,
             x0;
             epsilon=target_tolerance,
             memory_mode=FrankWolfe.OutplaceEmphasis(),
@@ -125,8 +125,8 @@ const f_opt = f(x_opt)
         callback(state) = push!(history, state)
 
         _, _, _ = FrankWolfe.adaptive_gradient_descent(
-            f,
-            grad!,
+            f_gd,
+            grad_gd!,
             x0;
             callback=callback,
             max_iteration=10,
@@ -148,8 +148,8 @@ const f_opt = f(x_opt)
 
         # Test with identity proximal operator (should match regular variant)
         x_id, f_id, _ = FrankWolfe.proximal_adaptive_gradient_descent(
-            f,
-            grad!,
+            f_gd,
+            grad_gd!,
             x0;
             epsilon=target_tolerance,
             verbose=false,
@@ -157,8 +157,8 @@ const f_opt = f(x_opt)
 
         # Test with L1 proximal operator
         x_l1, f_l1, _ = FrankWolfe.proximal_adaptive_gradient_descent(
-            f,
-            grad!,
+            f_gd,
+            grad_gd!,
             x0,
             ProximalOperators.IndBallL1(1.0);
             epsilon=target_tolerance,
@@ -168,15 +168,15 @@ const f_opt = f(x_opt)
 
         # Identity proximal operator should give same result as regular variant
         x_reg, f_reg, _ =
-            FrankWolfe.adaptive_gradient_descent(f, grad!, x0; epsilon=target_tolerance, verbose=false)
+            FrankWolfe.adaptive_gradient_descent(f_gd, grad_gd!, x0; epsilon=target_tolerance, verbose=false)
 
         @testset "Comparison with FW variants" begin
             @testset "L1-ball comparison" begin
                 x0 =
                     FrankWolfe.compute_extreme_point(FrankWolfe.LpNormLMO{Float64,1}(1.0), zeros(n))
                 x_fw_l1, _ = FrankWolfe.blended_pairwise_conditional_gradient(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     FrankWolfe.LpNormLMO{Float64,1}(1.0),
                     line_search=FrankWolfe.Secant(),
                     x0;
@@ -184,14 +184,14 @@ const f_opt = f(x_opt)
                     max_iteration=k,
                     verbose=false,
                 )
-                f_fw_l1 = f(x_fw_l1)
+                f_fw_l1 = f_gd(x_fw_l1)
                 @test abs(f_l1 - f_fw_l1) ≤ target_tolerance * 10
             end
 
             @testset "Probability simplex comparison" begin
                 x_prox_prob, f_prox_prob, _ = FrankWolfe.proximal_adaptive_gradient_descent(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     x0,
                     ProximalOperators.IndSimplex(1.0);
                     epsilon=target_tolerance,
@@ -203,8 +203,8 @@ const f_opt = f(x_opt)
                     zeros(n),
                 )
                 x_fw_prob, _ = FrankWolfe.blended_pairwise_conditional_gradient(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     FrankWolfe.ProbabilitySimplexOracle(1.0),
                     line_search=FrankWolfe.Secant(),
                     x0;
@@ -212,7 +212,7 @@ const f_opt = f(x_opt)
                     max_iteration=k,
                     verbose=false,
                 )
-                f_fw_prob = f(x_fw_prob)
+                f_fw_prob = f_gd(x_fw_prob)
                 @test abs(f_prox_prob - f_fw_prob) ≤ target_tolerance * 10
             end
             # TODO add unit simplex comparison once it is merged in ProximalOperators
@@ -220,8 +220,8 @@ const f_opt = f(x_opt)
             @testset "Box comparison" begin
                 τ_box = 1.0
                 x_prox_box, f_prox_box, _ = FrankWolfe.proximal_adaptive_gradient_descent(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     x0,
                     ProximalOperators.IndBox(0.0, τ_box);
                     epsilon=target_tolerance,
@@ -231,8 +231,8 @@ const f_opt = f(x_opt)
                 lmo_box = FrankWolfe.ScaledBoundLInfNormBall(zeros(n), τ_box * ones(n))
                 x0 = FrankWolfe.compute_extreme_point(lmo_box, zeros(n))
                 x_fw_box, _ = FrankWolfe.blended_pairwise_conditional_gradient(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     lmo_box,
                     line_search=FrankWolfe.Secant(),
                     x0;
@@ -240,14 +240,14 @@ const f_opt = f(x_opt)
                     max_iteration=k,
                     verbose=false,
                 )
-                f_fw_box = f(x_fw_box)
+                f_fw_box = f_gd(x_fw_box)
                 @test abs(f_prox_box - f_fw_box) ≤ target_tolerance * 10
             end
         end
 
         # Test convergence for identity proximal operator
         g0 = similar(x_id)
-        grad!(g0, x_id)
+        grad_gd!(g0, x_id)
         @test norm(g0) ≤ 1.5 * target_tolerance
 
         # Test whether identity and regular variant give same result
@@ -272,9 +272,9 @@ const f_opt = f(x_opt)
         # Make linear term negative to push solution into positive orthant
         linear_pos = -10.0 * ones(n)
 
-        f(x) = dot(linear_pos, x) + 0.5 * transpose(x) * hessian_pos * x
+        f_gd(x) = dot(linear_pos, x) + 0.5 * transpose(x) * hessian_pos * x
 
-        function grad!(storage, x)
+        function grad_gd!(storage, x)
             return storage .= linear_pos + hessian_pos * x
         end
 
@@ -282,7 +282,7 @@ const f_opt = f(x_opt)
 
         # Compute optimal solution using direct solve
         x_opt_pos = -hessian_pos \ linear_pos
-        f_opt_pos = f(x_opt_pos)
+        f_opt_pos = f_gd(x_opt_pos)
 
         # Testing proximal gradient descent with positive orthant solution
 
@@ -291,8 +291,8 @@ const f_opt = f(x_opt)
 
         # Test with identity proximal operator (should match regular variant)
         x_id, f_id, _ = FrankWolfe.proximal_adaptive_gradient_descent(
-            f,
-            grad!,
+            f_gd,
+            grad_gd!,
             x0;
             epsilon=target_tolerance,
             print_iter=print_iter,
@@ -301,8 +301,8 @@ const f_opt = f(x_opt)
 
         # Test with L1 proximal operator
         x_l1, f_l1, _ = FrankWolfe.proximal_adaptive_gradient_descent(
-            f,
-            grad!,
+            f_gd,
+            grad_gd!,
             x0,
             ProximalOperators.IndBallL1(1.0);
             epsilon=target_tolerance,
@@ -310,15 +310,15 @@ const f_opt = f(x_opt)
 
         # Identity proximal operator should give same result as regular variant
         x_reg, f_reg, _ =
-            FrankWolfe.adaptive_gradient_descent(f, grad!, x0; epsilon=target_tolerance)
+            FrankWolfe.adaptive_gradient_descent(f_gd, grad_gd!, x0; epsilon=target_tolerance)
 
         @testset "Comparison with FW variants" begin
             @testset "L1-ball comparison" begin
                 x0 =
                     FrankWolfe.compute_extreme_point(FrankWolfe.LpNormLMO{Float64,1}(1.0), zeros(n))
                 x_fw_l1, _ = FrankWolfe.blended_pairwise_conditional_gradient(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     FrankWolfe.LpNormLMO{Float64,1}(1.0),
                     line_search=FrankWolfe.Secant(),
                     x0;
@@ -327,14 +327,14 @@ const f_opt = f(x_opt)
                     print_iter=print_iter,
                     verbose=false,
                 )
-                f_fw_l1 = f(x_fw_l1)
+                f_fw_l1 = f_gd(x_fw_l1)
                 @test abs(f_l1 - f_fw_l1) ≤ target_tolerance * 10
             end
 
             @testset "Probability simplex comparison" begin
                 x_prox_prob, f_prox_prob, _ = FrankWolfe.proximal_adaptive_gradient_descent(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     x0,
                     FrankWolfe.ProbabilitySimplexProx();
                     epsilon=target_tolerance,
@@ -342,15 +342,15 @@ const f_opt = f(x_opt)
                 lmo_probsimplex = FrankWolfe.ProbabilitySimplexOracle(1.0)
                 x0 = FrankWolfe.compute_extreme_point(lmo_probsimplex, zeros(n))
                 x_fw_prob, _ = FrankWolfe.blended_pairwise_conditional_gradient(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     lmo_probsimplex,
                     line_search=FrankWolfe.Secant(),
                     x0;
                     epsilon=target_tolerance,
                     max_iteration=k,
                 )
-                f_fw_prob = f(x_fw_prob)
+                f_fw_prob = f_gd(x_fw_prob)
                 @test abs(f_prox_prob - f_fw_prob) ≤ target_tolerance * 10
             end
 
@@ -359,8 +359,8 @@ const f_opt = f(x_opt)
             @testset "Box comparison" begin
                 τ_box = 1.0
                 x_prox_box, f_prox_box, _ = FrankWolfe.proximal_adaptive_gradient_descent(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     x0,
                     ProximalOperators.IndBox(-τ_box, τ_box);
                     epsilon=target_tolerance,
@@ -368,22 +368,22 @@ const f_opt = f(x_opt)
                 lmo_box = FrankWolfe.LpNormLMO{Float64,Inf}(τ_box)
                 x0 = FrankWolfe.compute_extreme_point(lmo_box, zeros(n))
                 x_fw_box, _ = FrankWolfe.blended_pairwise_conditional_gradient(
-                    f,
-                    grad!,
+                    f_gd,
+                    grad_gd!,
                     lmo_box,
                     line_search=FrankWolfe.Secant(),
                     x0;
                     epsilon=target_tolerance,
                     max_iteration=k,
                 )
-                f_fw_box = f(x_fw_box)
+                f_fw_box = f_gd(x_fw_box)
                 @test abs(f_prox_box - f_fw_box) ≤ target_tolerance * 10
             end
         end
 
         # Test convergence for identity proximal operator
         gtest = similar(x_id)
-        grad!(gtest, x_id)
+        grad_gd!(gtest, x_id)
         @test norm(gtest) ≤ 10 * target_tolerance
 
         # Test whether identity and regular variant give same result
