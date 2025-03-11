598edd463f0da789361eaacc8c4a691590a5474d
Mathieu Besançon <mathieu.besancon@gmail.com>
2025-03-11 20:30:54 +0100

    first commit corrective

---
+++ src/corrective_frankwolfe.jl
@@ -0,0 +1,271 @@
+
+"""
+    corrective_frankwolfe(f, grad!, lmo, corrective_step, active_set::AS; kwargs...)
+
+A corrective Frank-Wolfe variant with corrective step defined by `corrective_step`.
+
+A corrective FW algorithm alternates between a standard FW step at which a vertex is added to the active set and a corrective step at which an update is performed in the convex hull of current vertices.
+Examples of corrective FW algorithms include blended (pairwise) conditional gradients, away-step Frank-Wolfe, and fully-corrective Frank-Wolfe.
+"""
+function corrective_frankwolfe(
+    f,
+    grad!,
+    lmo,
+    corrective_step::CorrectiveStep,
+    active_set::AbstractActiveSet{AT,R};
+    line_search::LineSearchMethod=Adaptive(),
+    epsilon=1e-7,
+    max_iteration=10000,
+    print_iter=1000,
+    trajectory=false,
+    verbose=false,
+    memory_mode::MemoryEmphasis=InplaceEmphasis(),
+    gradient=nothing,
+    callback=nothing,
+    traj_data=[],
+    timeout=Inf,
+    renorm_interval=1000,
+    lazy=false,
+    linesearch_workspace=nothing,
+    lazy_tolerance=2.0,
+    weight_purge_threshold=weight_purge_threshold_default(R),
+    extra_vertex_storage=nothing,
+    add_dropped_vertices=false,
+    use_extra_vertex_storage=false,
+    recompute_last_vertex=true,
+) where {AT,R}
+
+    # format string for output of the algorithm
+    format_string = "%6s %13s %14e %14e %14e %14e %14e %14i\n"
+    headers = ("Type", "Iteration", "Primal", "Dual", "Dual Gap", "Time", "It/sec", "#ActiveSet")
+    function format_state(state, active_set, args...)
+        rep = (
+            steptype_string[Symbol(state.step_type)],
+            string(state.t),
+            Float64(state.primal),
+            Float64(state.primal - state.dual_gap),
+            Float64(state.dual_gap),
+            state.time,
+            state.t / state.time,
+            length(active_set),
+        )
+        return rep
+    end
+
+    if trajectory
+        callback = make_trajectory_callback(callback, traj_data)
+    end
+
+    if verbose
+        callback = make_print_callback(callback, print_iter, headers, format_string, format_state)
+    end
+
+    t = 0
+    compute_active_set_iterate!(active_set)
+    x = get_active_set_iterate(active_set)
+    primal = convert(eltype(x), Inf)
+    step_type = ST_REGULAR
+    time_start = time_ns()
+
+    d = similar(x)
+
+    if gradient === nothing
+        gradient = collect(x)
+    end
+
+    if verbose
+        println("\nCorrective Frank-Wolfe Algorithm with $(nameof(typeof(corrective_step))) correction.")
+        NumType = eltype(x)
+        println(
+            "MEMORY_MODE: $memory_mode STEPSIZE: $line_search EPSILON: $epsilon MAXITERATION: $max_iteration TYPE: $NumType",
+        )
+        grad_type = typeof(gradient)
+        println("GRADIENTTYPE: $grad_type LAZY: $lazy lazy_tolerance: $lazy_tolerance")
+        println("LMO: $(typeof(lmo))")
+    end
+
+    grad!(gradient, x)
+    v = compute_extreme_point(lmo, gradient)
+    # if !lazy, phi is maintained as the global dual gap
+    phi = max(0, fast_dot(x, gradient) - fast_dot(v, gradient))
+    gamma = one(phi)
+
+    if linesearch_workspace === nothing
+        linesearch_workspace = build_linesearch_workspace(line_search, x, gradient)
+    end
+
+    if extra_vertex_storage === nothing
+        use_extra_vertex_storage = add_dropped_vertices = false
+    end
+
+    while t <= max_iteration && phi >= max(epsilon, eps(epsilon))
+
+        # managing time limit
+        time_at_loop = time_ns()
+        if t == 0
+            time_start = time_at_loop
+        end
+        # time is measured at beginning of loop for consistency throughout all algorithms
+        tot_time = (time_at_loop - time_start) / 1e9
+
+        if timeout < Inf
+            if tot_time ≥ timeout
+                if verbose
+                    @info "Time limit reached"
+                end
+                break
+            end
+        end
+
+        #####################
+        t += 1
+
+        # compute current iterate from active set
+        x = get_active_set_iterate(active_set)
+        primal = f(x)
+        if t > 1
+            grad!(gradient, x)
+        end
+
+        should_compute_vertex = prepare_corrective_step(corrective_step, f, grad!, gradient, active_set, t, lmo, primal, phi)
+
+        if should_compute_vertex && t > 1
+            v = compute_extreme_point(lmo, gradient)
+            dual_gap = fast_dot(gradient, x) - fast_dot(gradient, v)
+            phi = dual_gap
+        end
+        # use the step defined by the corrective step type
+        x, v, phi, dual_gap, should_fw_step, should_continue = run_corrective_step(corrective_step, f, grad!, gradient, x, v, dual_gap, active_set, t, lmo, line_search, linesearch_workspace, primal, phi, tot_time, callback, renorm_interval, memory_mode, epsilon, lazy_tolerance, d)
+        # interrupt from callback
+        if should_continue === false
+            break
+        end
+        if should_fw_step
+            step_type = ST_REGULAR
+            # if we are about to exit, compute dual_gap with the cleaned-up x
+            if dual_gap ≤ epsilon
+                active_set_renormalize!(active_set)
+                active_set_cleanup!(active_set; weight_purge_threshold=weight_purge_threshold)
+                compute_active_set_iterate!(active_set)
+                x = get_active_set_iterate(active_set)
+                grad!(gradient, x)
+                dual_gap = fast_dot(gradient, x) - fast_dot(gradient, v)
+            end
+            if (dual_gap ≥ epsilon) && (!lazy || dual_gap ≥ phi / lazy_tolerance)
+                d = muladd_memory_mode(memory_mode, d, x, v)
+
+                gamma = perform_line_search(
+                    line_search,
+                    t,
+                    f,
+                    grad!,
+                    gradient,
+                    x,
+                    d,
+                    one(eltype(x)),
+                    linesearch_workspace,
+                    memory_mode,
+                )
+                if callback !== nothing
+                    state = CallbackState(
+                        t,
+                        primal,
+                        primal - phi,
+                        phi,
+                        tot_time,
+                        x,
+                        v,
+                        d,
+                        gamma,
+                        f,
+                        grad!,
+                        lmo,
+                        gradient,
+                        step_type,
+                    )
+                    if callback(state, active_set) === false
+                        break
+                    end
+                end
+
+                # dropping active set and restarting from singleton
+                if gamma ≈ 1.0
+                    active_set_initialize!(active_set, v)
+                else
+                    renorm = mod(t, renorm_interval) == 0
+                    active_set_update!(active_set, gamma, v, renorm, nothing)
+                end
+            end
+        end
+    end
+
+    # recompute everything once more for final verfication / do not record to trajectory though for now!
+    # this is important as some variants do not recompute f(x) and the dual_gap regularly but only when reporting
+    # hence the final computation.
+    # do also cleanup of active_set due to many operations on the same set
+
+    if verbose
+        compute_active_set_iterate!(active_set)
+        x = get_active_set_iterate(active_set)
+        grad!(gradient, x)
+        v = compute_extreme_point(lmo, gradient)
+        primal = f(x)
+        phi_new = fast_dot(x, gradient) - fast_dot(v, gradient)
+        phi = phi_new < phi ? phi_new : phi
+        step_type = ST_LAST
+        tot_time = (time_ns() - time_start) / 1e9
+        if callback !== nothing
+            state = CallbackState(
+                t,
+                primal,
+                primal - phi,
+                phi,
+                tot_time,
+                x,
+                v,
+                nothing,
+                gamma,
+                f,
+                grad!,
+                lmo,
+                gradient,
+                step_type,
+            )
+            callback(state, active_set)
+        end
+    end
+    active_set_renormalize!(active_set)
+    active_set_cleanup!(active_set; weight_purge_threshold=weight_purge_threshold)
+    compute_active_set_iterate!(active_set)
+    x = get_active_set_iterate(active_set)
+    grad!(gradient, x)
+    # otherwise values are maintained to last iteration
+    if recompute_last_vertex
+        v = compute_extreme_point(lmo, gradient)
+        primal = f(x)
+        dual_gap = fast_dot(x, gradient) - fast_dot(v, gradient)
+    end
+    step_type = ST_POSTPROCESS
+    tot_time = (time_ns() - time_start) / 1e9
+    if callback !== nothing
+        state = CallbackState(
+            t,
+            primal,
+            primal - dual_gap,
+            dual_gap,
+            tot_time,
+            x,
+            v,
+            nothing,
+            gamma,
+            f,
+            grad!,
+            lmo,
+            gradient,
+            step_type,
+        )
+        callback(state, active_set)
+    end
+
+    return (x=x, v=v, primal=primal, dual_gap=dual_gap, traj_data=traj_data, active_set=active_set)
+end
---
+++ src/corrective_step_interface.jl
@@ -0,0 +1,139 @@
+
+abstract type CorrectiveStep end
+
+"""
+    run_corrective_step(corrective_step, f, grad!, gradient, x, active_set, t, lmo, line_search, linesearch_workspace, primal, phi, tot_time, callback, renorm_interval) -> (x, phi, primal)
+
+Corrective step method specific to the `CS` corrective_step type.
+The corrective step can perform whatever update over the current active set, the function should return the new iterate  a FW step should be run next with the boolean `should_fw_step` and compute a new dual gap estimate `phi`.
+"""
+function run_corrective_step end
+
+"""
+    prepare_corrective_step(corrective_step::CS, f, grad!, gradient, active_set, t, lmo, primal, phi, tot_time) -> (should_compute_vertex, use_corrective)
+
+`should_compute_vertex` is a boolean flag deciding whether a new vertex should be computed.
+`use_corrective` is a function that takes the vertex (the vertex is a valid new vertex only if should_compute_vertex was true)
+"""
+function prepare_corrective_step end
+
+"""
+    (Lazified) away-step for corrective Frank-Wolfe
+"""
+struct AwayStep <: CorrectiveStep
+    lazy::Bool
+end
+
+AwayStep() = AwayStep(false)
+
+function prepare_corrective_step(corrective_step::AwayStep, f, grad!, gradient, active_set, t, lmo, primal, phi, tot_time)
+    should_compute_vertex = !corrective_step.lazy
+    return should_compute_vertex
+end
+
+function run_corrective_step(corrective_step::AwayStep, f, grad!, gradient, x, v, dual_gap, active_set, t, lmo, line_search, linesearch_workspace, primal, phi, tot_time, callback, renorm_interval, memory_mode, epsilon, lazy_tolerance)
+    _, v_lazy, v_loc, _, a_lambda, a, a_loc, _, _ = active_set_argminmax(active_set, gradient)
+    grad_dot_x = fast_dot(x, gradient)
+    grad_dot_a = fast_dot(a, gradient)
+    away_gap = grad_dot_a - grad_dot_x
+    new_phi = phi
+    # flag for whether callback interrupts the solving process
+    should_continue = true
+    if !corrective_step.lazy
+        if away_gap >= dual_gap
+            gamma_max = a_lambda / (1 - a_lambda)
+            d = muladd_memory_mode(memory_mode, d, a, x)
+            gamma = perform_line_search(
+                line_search,
+                t,
+                f,
+                grad!,
+                gradient,
+                x,
+                d,
+                gamma_max,
+                linesearch_workspace,
+                memory_mode,
+            )
+            gamma = min(gamma_max, gamma)
+            step_type = gamma ≈ gamma_max ? ST_DROP : ST_AWAY
+            should_fw_step = false
+            callback_state = CallbackState(
+                t,
+                primal,
+                primal - new_phi,
+                new_phi,
+                tot_time,
+                x,
+                v,
+                d,
+                gamma,
+                f,
+                grad!,
+                lmo,
+                gradient,
+                step_type,
+            )
+            if callback !== nothing
+                should_continue = callback(callback_state)
+            end
+            active_set_update!(active_set, -gamma, a, true, a_loc)        
+        else
+            should_fw_step = true
+        end
+    else # lazy AFW
+        # compute the local FW gap over the active set 
+        away_step_taken = false
+        fw_step_taken = false
+        grad_dot_lazy_fw_vertex = fast_dot(v_lazy, gradient)
+        lazy_gap = grad_dot_x - grad_dot_lazy_fw_vertex
+        if lazy_gap >= max(away_gap, phi / lazy_tolerance, epsilon)
+            step_type = ST_LAZY
+            gamma_max = one(a_lambda)
+            d = muladd_memory_mode(memory_mode, d, x, v_lazy)
+            vertex = v
+            fw_step_taken = true
+            index = v_loc
+            should_fw_step = false
+        elseif away_gap >= max(phi / lazy_tolerance, epsilon)
+            step_type = ST_AWAY
+            gamma_max = a_lambda / (1 - a_lambda)
+            d = muladd_memory_mode(memory_mode, d, a, x)
+            vertex = a
+            away_step_taken = true
+            index = a_loc
+            should_fw_step = false
+        else
+            # call the true LMO since `v` was not updated
+            step_type = ST_REGULAR
+            v = compute_extreme_point(lmo, gradient)
+            grad_dot_fw_vertex = fast_dot(v, gradient)
+            dual_gap = grad_dot_x - grad_dot_fw_vertex
+            should_fw_step = true
+        end
+        if fw_step_taken || away_step_taken
+            gamma = perform_line_search(
+                line_search,
+                t,
+                f,
+                grad!,
+                gradient,
+                x,
+                d,
+                gamma_max,
+                linesearch_workspace,
+                memory_mode,
+            )
+            gamma = min(gamma_max, gamma)
+            step_type = gamma ≈ gamma_max ? ST_DROP : step_type
+            # cleanup and renormalize every x iterations. Only for the fw steps.
+            renorm = mod(t, renorm_interval) == 0
+            if away_step_taken
+                active_set_update!(active_set, -gamma, vertex, true, index)
+            else
+                active_set_update!(active_set, gamma, vertex, renorm, index)
+            end
+        end
+    end
+    return x, v, phi, dual_gap, should_fw_step, should_continue
+end
