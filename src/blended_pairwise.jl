8da91e9d88ba8d8f0934170cb5ab009796b8bdaa
Mathieu Besançon <mathieu.besancon@gmail.com>
2025-03-18 11:26:31 +0100

    wrapped the pairwise update in a dedicated function (#557)
    * wrapped the pairwise update in a dedicated function
    
    * Add active_set_add_weight!
    
    * Add active_set_mul_weights!
    
    * Add @inbounds in active_set_add_weight!
    
    * Simplify active_set_renormalize!
    
    ---------
    
    Co-authored-by: Sébastien Designolle <sebastien.designolle@gmail.com>

--- src/blended_pairwise.jl
+++ src/blended_pairwise.jl
@@ -249,19 +249,7 @@ function blended_pairwise_conditional_gradient(
                     break
                 end
             end
-            # reached maximum of lambda -> dropping away vertex
-            if gamma ≈ gamma_max
-                active_set.weights[v_local_loc] += gamma
-                deleteat!(active_set, a_loc)
-                if add_dropped_vertices
-                    push!(extra_vertex_storage, a)
-                end
-            else # transfer weight from away to local FW
-                active_set.weights[a_loc] -= gamma
-                active_set.weights[v_local_loc] += gamma
-                @assert active_set_validate(active_set)
-            end
-            active_set_update_iterate_pairwise!(active_set.x, gamma, v_local, a)
+            active_set_update_pairwise!(active_set, gamma, gamma_max, v_local_loc, a_loc, v_local, a, add_dropped_vertices, extra_vertex_storage)
         else # add to active set
             if lazy # otherwise, v computed above already
                 # optionally try to use the storage
