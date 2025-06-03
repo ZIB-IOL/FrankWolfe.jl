0b4171ee6d4ef903e3ad907e6a12cffb7daeef64
Deborah Hendrych <d.hendrych@web.de>
2025-06-03 15:13:57 +0200

    Delete commented code.

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,54 +27,33 @@ using DoubleFloats
     include("tracking.jl")
 
     # in separate module for name space issues
-    #module BCGDirectionError
-    #using Test
     @testset "BCG direction accuracy" begin
         include("bcg_direction_error.jl")
     end
-    #end
 
-    #module RationalTest
-    #using Test
     @testset "Rational test and shortstep" begin
         include("rational_test.jl")
     end
-    #end
 
-    #module BCGAcceleration
-    #using Test
     @testset "BCG acceleration with different types" begin
         include("blended_accelerated.jl")
     end
-    #end
 
-    #module VertexStorageTest
-    #using Test
     @testset "Vertex storage" begin
         include("extra_storage.jl")
     end
-    #end
 
-    #module LpDirectSolveTest
-    #using Test
     @testset "LP solving for quadratic functions and active set" begin
         include("quadratic_lp_active_set.jl")
     end
-    #end
 
-    #module LpDirectSolveTestProjection
-    #using Test
     @testset "LP solving for standard quadratic function and active set" begin
         include("as_quadratic_projection.jl")
     end
-    #end
 
-    #module SparsifyingActiveSetTest
-    #using Test
     @testset "Sparsifying active set" begin
         include("sparsifying_activeset.jl")
     end
-    #end
 
     include("generic-arrays.jl")
 
