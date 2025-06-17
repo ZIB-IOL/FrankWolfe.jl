3b3a7cb8d3d920d9e8af26b636632669345f3cc4
Deborah Hendrych <d.hendrych@web.de>
2025-06-17 23:18:52 +0200

    Add Aqua to the tests.

---
+++ test/aqua.jl
@@ -0,0 +1,14 @@
+using Aqua
+using FrankWolfe
+using Test
+
+@testset "Aqua.jl" begin
+    Aqua.test_all(
+        FrankWolfe;
+        # You can customize which tests to run and their options
+        # ambiguities=(exclude=[SomePackage.some_function], broken=true),
+        # stale_deps=(ignore=[:SomePackage],),
+        # deps_compat=(ignore=[:SomeOtherPackage],),
+        # piracies=false,
+    )
+end 
