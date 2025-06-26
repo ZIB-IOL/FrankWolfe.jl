7b6a24c2ed1df0f83dbcccbb8dae46b227c3b84b
matbesancon <mathieu.besancon@gmail.com>
2025-06-26 17:14:29 +0200

    unbound args remain broken

--- test/aqua.jl
+++ test/aqua.jl
@@ -6,6 +6,7 @@ using Test
     Aqua.test_all(
         FrankWolfe;
         # You can customize which tests to run and their options
+        unbound_args=false,
         # ambiguities=(exclude=[SomePackage.some_function], broken=true),
         # stale_deps=(ignore=[:SomePackage],),
         # deps_compat=(ignore=[:SomeOtherPackage],),
