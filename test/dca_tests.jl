79d3f319a263ee1b452a74b97ac556ea393d8f7a
matbesancon <mathieu.besancon@gmail.com>
2025-08-20 14:34:40 +0200

    fix call

--- test/dca_tests.jl
+++ test/dca_tests.jl
@@ -360,7 +360,7 @@ using StableRNGs
         result =
             FrankWolfe.dca_fw(f, grad_f!, g, grad_g!, lmo, copy(x0), max_iteration=1, verbose=false)
         @test result.iterations <= 1
-        @test result.status == STATUS_MAXITER
+        @test result.status == FrankWolfe.STATUS_MAXITER
 
         # Test with very loose tolerance
         result = FrankWolfe.dca_fw(
