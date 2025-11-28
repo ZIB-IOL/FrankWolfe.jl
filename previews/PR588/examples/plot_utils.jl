44923ef38d1fe2895fa2adb421abca4c6e0b7e71
Documenter.jl <documenter@juliadocs.github.io>
2025-11-28 16:52:24 +0000

    build based on 01651dd

---
+++ previews/PR588/examples/plot_utils.jl
@@ -0,0 +1,432 @@
+using Plots
+
+"""
+plot_results
+Given a series of list, generate subplots.
+list_data_y -> contains a list of a list of lists (where each list refers to a subplot, and a list of lists refers to the y-values of the series inside a subplot).
+list_data_x -> contains a list of a list of lists (where each list refers to a subplot, and a list of lists refers to the x-values of the series inside a subplot).
+So if we have one plot with two series, these might look like:
+    list_data_y = [[[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]]]
+    list_data_x = [[[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]]]
+And if we have two plots, each with two series, these might look like:
+    list_data_y = [[[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]], [[7, 8, 9, 10, 11, 12], [7, 8, 9, 10, 11, 12]]]
+    list_data_x = [[[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]], [[7, 8, 9, 10, 11, 12], [7, 8, 9, 10, 11, 12]]]
+list_label -> contains the labels for the series that will be plotted,
+which has to have a length equal to the number of series that are being plotted:
+    list_label = ["Series 1", "Series 2"]
+list_axis_x -> contains the labels for the x-axis that will be plotted,
+which has to have a length equal to the number of subplots:
+    list_axis_x = ["x-axis plot 1", "x-axis plot 1"]
+list_axis_y -> Same as list_axis_x but for the y-axis
+xscalelog -> A list of values indicating the type of axes to use in each subplot,
+must be equal to the number of subplots:
+    xscalelog = [:log, :identity]
+yscalelog -> Same as xscalelog but for the y-axis
+"""
+function plot_results(
+    list_data_y,
+    list_data_x,
+    list_label,
+    list_axis_x,
+    list_axis_y;
+    filename=nothing,
+    xscalelog=nothing,
+    yscalelog=nothing,
+    legend_position=nothing,
+    list_style=fill(:solid, length(list_label)),
+    list_color=get_color_palette(:auto, plot_color(:white)),
+    list_markers=[
+        :circle,
+        :rect,
+        :utriangle,
+        :diamond,
+        :hexagon,
+        :+,
+        :x,
+        :star5,
+        :cross,
+        :xcross,
+        :dtriangle,
+        :rtriangle,
+        :ltriangle,
+        :pentagon,
+        :heptagon,
+        :octagon,
+        :star4,
+        :star6,
+        :star7,
+        :star8,
+        :vline,
+        :hline,
+    ],
+    number_markers_per_line=10,
+    line_width=3.0,
+    marker_size=5.0,
+    transparency_markers=0.45,
+    font_size_axis=12,
+    font_size_legend=9,
+)
+    gr()
+    plt = nothing
+    list_plots = Plots.Plot{Plots.GRBackend}[]
+    #Plot an appropiate number of plots
+    for i in eachindex(list_data_x)
+        for j in eachindex(list_data_x[i])
+            if isnothing(xscalelog)
+                xscale = :identity
+            else
+                xscale = xscalelog[i]
+            end
+            if isnothing(yscalelog)
+                yscale = :log
+            else
+                yscale = yscalelog[i]
+            end
+            if isnothing(legend_position)
+                position_legend = :best
+                legend_display = true
+            else
+                position_legend = legend_position[i]
+                if isnothing(position_legend)
+                    legend_display = false
+                else
+                    legend_display = true
+                end
+            end
+            if j == 1
+                if legend_display
+                    plt = plot(
+                        list_data_x[i][j],
+                        list_data_y[i][j],
+                        label="",
+                        xaxis=xscale,
+                        yaxis=yscale,
+                        ylabel=list_axis_y[i],
+                        xlabel=list_axis_x[i],
+                        legend=position_legend,
+                        yguidefontsize=font_size_axis,
+                        xguidefontsize=font_size_axis,
+                        legendfontsize=font_size_legend,
+                        width=line_width,
+                        linestyle=list_style[j],
+                        color=list_color[j],
+                        grid=true,
+                    )
+                else
+                    plt = plot(
+                        list_data_x[i][j],
+                        list_data_y[i][j],
+                        label="",
+                        xaxis=xscale,
+                        yaxis=yscale,
+                        ylabel=list_axis_y[i],
+                        xlabel=list_axis_x[i],
+                        yguidefontsize=font_size_axis,
+                        xguidefontsize=font_size_axis,
+                        width=line_width,
+                        linestyle=list_style[j],
+                        color=list_color[j],
+                        grid=true,
+                    )
+                end
+            else
+                if legend_display
+                    plot!(
+                        list_data_x[i][j],
+                        list_data_y[i][j],
+                        label="",
+                        width=line_width,
+                        linestyle=list_style[j],
+                        color=list_color[j],
+                        legend=position_legend,
+                    )
+                else
+                    plot!(
+                        list_data_x[i][j],
+                        list_data_y[i][j],
+                        label="",
+                        width=line_width,
+                        linestyle=list_style[j],
+                        color=list_color[j],
+                    )
+                end
+            end
+            if xscale == :log
+                indices =
+                    round.(
+                        Int,
+                        10 .^ (range(
+                            log10(1),
+                            log10(length(list_data_x[i][j])),
+                            length=number_markers_per_line,
+                        )),
+                    )
+                scatter!(
+                    list_data_x[i][j][indices],
+                    list_data_y[i][j][indices],
+                    markershape=list_markers[j],
+                    markercolor=list_color[j],
+                    markersize=marker_size,
+                    markeralpha=transparency_markers,
+                    label=list_label[j],
+                    legend=position_legend,
+                )
+            else
+                scatter!(
+                    view(
+                        list_data_x[i][j],
+                        1:length(list_data_x[i][j])÷number_markers_per_line:length(
+                            list_data_x[i][j],
+                        ),
+                    ),
+                    view(
+                        list_data_y[i][j],
+                        1:length(list_data_y[i][j])÷number_markers_per_line:length(
+                            list_data_y[i][j],
+                        ),
+                    ),
+                    markershape=list_markers[j],
+                    markercolor=list_color[j],
+                    markersize=marker_size,
+                    markeralpha=transparency_markers,
+                    label=list_label[j],
+                    legend=position_legend,
+                )
+            end
+        end
+        push!(list_plots, plt)
+    end
+    fp = plot(list_plots..., layout=length(list_plots))
+    plot!(size=(600, 400))
+    if filename !== nothing
+        savefig(fp, filename)
+    end
+    return fp
+end
+
+# Recipe for plotting markers in plot_trajectories
+@recipe function f(::Type{Val{:samplemarkers}}, x, y, z; n_markers=10, log=false)
+    n = length(y)
+
+    # Choose datapoints for markers
+    if log
+        xmin = log10(x[1])
+        xmax = log10(x[end])
+        thresholds = collect(xmin:(xmax-xmin)/(n_markers-1):xmax)
+        indices = [argmin(i -> abs(t - log10(x[i])), eachindex(x)) for t in thresholds]
+    else
+        indices = 1:Int(ceil(length(x) / n_markers)):n
+    end
+    sx, sy = x[indices], y[indices]
+
+    # add an empty series with the correct type for legend markers
+    @series begin
+        seriestype := :path
+        markershape --> :auto
+        x := []
+        y := []
+    end
+    # add a series for the line
+    @series begin
+        primary := false # no legend entry
+        markershape := :none # ensure no markers
+        seriestype := :path
+        seriescolor := get(plotattributes, :seriescolor, :auto)
+        x := x
+        y := y
+    end
+    # return  a series for the sampled markers
+    primary := false
+    seriestype := :scatter
+    markershape --> :auto
+    x := sx
+    y := sy
+    z_order := 1
+end
+
+function plot_trajectories(
+    data,
+    label;
+    filename=nothing,
+    xscalelog=false,
+    yscalelog=true,
+    legend_position=:topright,
+    lstyle=fill(:solid, length(data)),
+    marker_shapes=nothing,
+    n_markers=10,
+    reduce_size=false,
+    primal_offset=1e-8,
+    line_width=1.3,
+    empty_marker=false,
+    extra_plot=false,
+    extra_plot_label="",
+    plot_title="",
+)
+    # theme(:dark)
+    # theme(:vibrant)
+    Plots.gr()
+
+    x = []
+    y = []
+    offset = 2
+
+    function sub_plot(idx_x, idx_y; legend=false, xlabel="", ylabel="", y_offset=0)
+
+        fig = nothing
+
+        for (i, trajectory) in enumerate(data)
+
+            l = length(trajectory)
+
+            if reduce_size && l > 1000
+                if xscalelog
+                    xmin = log10(offset)
+                    xmax = log10(l)
+                    indices = Int.(round.(10.0 .^ (collect(xmin:(xmax-xmin)/1000:xmax))))
+                else
+                    indices = Int.(round.(collect(offset:l/1000:l)))
+                end
+                trajectory = trajectory[indices]
+            else
+                trajectory = trajectory[offset:end]
+            end
+
+            x = [trajectory[j][idx_x] for j in eachindex(trajectory)]
+            y = [trajectory[j][idx_y] + y_offset for j in eachindex(trajectory)]
+
+            if marker_shapes !== nothing && n_markers >= 2
+                marker_args = Dict(
+                    :st => :samplemarkers,
+                    :n_markers => n_markers,
+                    :shape => marker_shapes[i],
+                    :log => xscalelog,
+                    :markercolor => empty_marker ? :white : :match,
+                    :markerstrokecolor => empty_marker ? i : :match,
+                )
+            else
+                marker_args = Dict()
+            end
+
+            if i == 1
+                fig = plot(
+                    x,
+                    y,
+                    label=label[i],
+                    xaxis=xscalelog ? :log : :identity,
+                    yaxis=yscalelog ? :log : :identity,
+                    xlabel=xlabel,
+                    ylabel=ylabel,
+                    legend=legend,
+                    yguidefontsize=8,
+                    xguidefontsize=8,
+                    legendfontsize=8,
+                    width=line_width,
+                    linestyle=lstyle[i];
+                    marker_args...,
+                )
+            else
+                plot!(x, y, label=label[i], width=line_width, linestyle=lstyle[i]; marker_args...)
+            end
+        end
+        return fig
+    end
+
+    pit = sub_plot(1, 2; legend=legend_position, ylabel="Primal", y_offset=primal_offset)
+    pti = sub_plot(5, 2; y_offset=primal_offset)
+    dit = sub_plot(1, 4; xlabel="Iterations", ylabel="FW gap")
+    dti = sub_plot(5, 4; xlabel="Time (s)")
+
+    if extra_plot
+        iit = sub_plot(1, 6; ylabel=extra_plot_label)
+        iti = sub_plot(5, 6)
+        fp = plot(pit, pti, iit, iti, dit, dti, layout=(3, 2), plot_title=plot_title) # layout = @layout([A{0.01h}; [B C; D E]]))
+        plot!(size=(600, 600))
+    else        
+        fp = plot(pit, pti, dit, dti, layout=(2, 2), plot_title=plot_title) # layout = @layout([A{0.01h}; [B C; D E]]))
+        plot!(size=(600, 400))
+    end
+    if filename !== nothing
+        savefig(fp, filename)
+    end
+    return fp
+end
+
+function plot_sparsity(
+    data,
+    label;
+    filename=nothing,
+    xscalelog=false,
+    legend_position=:topright,
+    yscalelog=true,
+    lstyle=fill(:solid, length(data)),
+    marker_shapes=nothing,
+    n_markers=10,
+    empty_marker=false,
+    reduce_size=false,
+)
+    Plots.gr()
+
+    xscale = xscalelog ? :log : :identity
+    yscale = yscalelog ? :log : :identity
+    offset = 2
+
+    function subplot(idx_x, idx_y, ylabel)
+
+        fig = nothing
+        for (i, trajectory) in enumerate(data)
+
+            l = length(trajectory)
+            if reduce_size && l > 1000
+                indices = Int.(round.(collect(1:l/1000:l)))
+                trajectory = trajectory[indices]
+            end
+
+
+            x = [trajectory[j][idx_x] for j in offset:length(trajectory)]
+            y = [trajectory[j][idx_y] for j in offset:length(trajectory)]
+            if marker_shapes !== nothing && n_markers >= 2
+                marker_args = Dict(
+                    :st => :samplemarkers,
+                    :n_markers => n_markers,
+                    :shape => marker_shapes[i],
+                    :log => xscalelog,
+                    :startmark => 5 + 20 * (i - 1),
+                    :markercolor => empty_marker ? :white : :match,
+                    :markerstrokecolor => empty_marker ? i : :match,
+                )
+            else
+                marker_args = Dict()
+            end
+            if i == 1
+                fig = plot(
+                    x,
+                    y;
+                    label=label[i],
+                    xaxis=xscale,
+                    yaxis=yscale,
+                    ylabel=ylabel,
+                    legend=legend_position,
+                    yguidefontsize=8,
+                    xguidefontsize=8,
+                    legendfontsize=8,
+                    linestyle=lstyle[i],
+                    marker_args...,
+                )
+            else
+                plot!(x, y; label=label[i], linestyle=lstyle[i], marker_args...)
+            end
+        end
+
+        return fig
+    end
+
+    ps = subplot(6, 2, "Primal")
+    ds = subplot(6, 4, "FW gap")
+
+    fp = plot(ps, ds, layout=(1, 2)) # layout = @layout([A{0.01h}; [B C; D E]]))
+    plot!(size=(600, 200))
+    if filename !== nothing
+        savefig(fp, filename)
+    end
+    return fp
+end
---
+++ v0.1.18/examples/3_matrix_completion.jl
@@ -0,0 +1,282 @@
+# # Matrix Completion
+
+# We present another example that is about matrix completion. The idea is, given a partially observed matrix ``Y\in\mathbb{R}^{m\times n}``, to find
+# ``X\in\mathbb{R}^{m\times n}`` to minimize the sum of squared errors from the observed entries while 'completing' the matrix ``Y``, i.e. filling the unobserved
+# entries to match ``Y`` as good as possible. A detailed explanation can be found in section 4.2 of
+# [the paper](https://arxiv.org/pdf/2104.06675.pdf).
+# We will try to solve
+# ```math
+# \min_{||X||_*\le \tau} \sum_{(i,j)\in\mathcal{I}} (X_{i,j}iY_{i,j})^2,
+# ```
+# where ``\tau>0`` and ``\mathcal{I}`` denote the indices of the observed entries. We will use [`FrankWolfe.NuclearNormLMO`](@ref) and compare our
+# Frank-Wolfe implementation with a Projected Gradient Descent (PGD) algorithm which, after each gradient descent step, projects the iterates back onto the nuclear
+# norm ball. We use a movielens dataset for comparison.
+
+using FrankWolfe
+using ZipFile, DataFrames, CSV
+
+using Random
+using Plots
+
+using Profile
+
+import Arpack
+using SparseArrays, LinearAlgebra
+
+using LaTeXStrings
+
+temp_zipfile = download("http://files.grouplens.org/datasets/movielens/ml-latest-small.zip")
+
+zarchive = ZipFile.Reader(temp_zipfile)
+
+movies_file = zarchive.files[findfirst(f -> occursin("movies", f.name), zarchive.files)]
+movies_frame = CSV.read(movies_file, DataFrame)
+
+ratings_file = zarchive.files[findfirst(f -> occursin("ratings", f.name), zarchive.files)]
+ratings_frame = CSV.read(ratings_file, DataFrame)
+
+users = unique(ratings_frame[:, :userId])
+movies = unique(ratings_frame[:, :movieId])
+
+@assert users == eachindex(users)
+movies_revert = zeros(Int, maximum(movies))
+for (idx, m) in enumerate(movies)
+    movies_revert[m] = idx
+end
+movies_indices = [movies_revert[idx] for idx in ratings_frame[:, :movieId]]
+
+const rating_matrix = sparse(
+    ratings_frame[:, :userId],
+    movies_indices,
+    ratings_frame[:, :rating],
+    length(users),
+    length(movies),
+)
+
+missing_rate = 0.05
+
+Random.seed!(42)
+
+const missing_ratings = Tuple{Int,Int}[]
+const present_ratings = Tuple{Int,Int}[]
+let
+    (I, J, V) = SparseArrays.findnz(rating_matrix)
+    for idx in eachindex(I)
+        if V[idx] > 0
+            if rand() <= missing_rate
+                push!(missing_ratings, (I[idx], J[idx]))
+            else
+                push!(present_ratings, (I[idx], J[idx]))
+            end
+        end
+    end
+end
+
+function f(X)
+    r = 0.0
+    for (i, j) in present_ratings
+        r += 0.5 * (X[i, j] - rating_matrix[i, j])^2
+    end
+    return r
+end
+
+function grad!(storage, X)
+    storage .= 0
+    for (i, j) in present_ratings
+        storage[i, j] = X[i, j] - rating_matrix[i, j]
+    end
+    return nothing
+end
+
+function test_loss(X)
+    r = 0.0
+    for (i, j) in missing_ratings
+        r += 0.5 * (X[i, j] - rating_matrix[i, j])^2
+    end
+    return r
+end
+
+function project_nuclear_norm_ball(X; radius=1.0)
+    U, sing_val, Vt = svd(X)
+    if (sum(sing_val) <= radius)
+        return X, -norm_estimation * U[:, 1] * Vt[:, 1]'
+    end
+    sing_val = FrankWolfe.projection_simplex_sort(sing_val, s=radius)
+    return U * Diagonal(sing_val) * Vt', -norm_estimation * U[:, 1] * Vt[:, 1]'
+end
+
+norm_estimation = 10 * Arpack.svds(rating_matrix, nsv=1, ritzvec=false)[1].S[1]
+
+const lmo = FrankWolfe.NuclearNormLMO(norm_estimation)
+const x0 = FrankWolfe.compute_extreme_point(lmo, ones(size(rating_matrix)))
+const k = 10
+
+FrankWolfe.benchmark_oracles(
+    f,
+    (str, x) -> grad!(str, x),
+    () -> randn(size(rating_matrix)),
+    lmo;
+    k=10,
+)
+
+gradient = spzeros(size(x0)...)
+gradient_aux = spzeros(size(x0)...)
+
+function build_callback(trajectory_arr)
+    return function callback(state)
+        return push!(trajectory_arr, (Tuple(state)[1:5]..., test_loss(state.x)))
+    end
+end
+
+# The smoothness constant is estimated:
+
+num_pairs = 100
+L_estimate = -Inf
+for i in 1:num_pairs
+    global L_estimate
+    u1 = rand(size(x0, 1))
+    u1 ./= sum(u1)
+    u1 .*= norm_estimation
+    v1 = rand(size(x0, 2))
+    v1 ./= sum(v1)
+    x = FrankWolfe.RankOneMatrix(u1, v1)
+    u2 = rand(size(x0, 1))
+    u2 ./= sum(u2)
+    u2 .*= norm_estimation
+    v2 = rand(size(x0, 2))
+    v2 ./= sum(v2)
+    y = FrankWolfe.RankOneMatrix(u2, v2)
+    grad!(gradient, x)
+    grad!(gradient_aux, y)
+    new_L = norm(gradient - gradient_aux) / norm(x - y)
+    if new_L > L_estimate
+        L_estimate = new_L
+    end
+end
+
+# We can now perform projected gradient descent:
+
+xgd = Matrix(x0)
+function_values = Float64[]
+timing_values = Float64[]
+function_test_values = Float64[]
+
+time_start = time_ns()
+for _ in 1:k
+    f_val = f(xgd)
+    push!(function_values, f_val)
+    push!(function_test_values, test_loss(xgd))
+    push!(timing_values, (time_ns() - time_start) / 1e9)
+    @info f_val
+    grad!(gradient, xgd)
+    xgd_new, vertex = project_nuclear_norm_ball(xgd - gradient / L_estimate, radius=norm_estimation)
+    gamma, _ = FrankWolfe.backtrackingLS(f, gradient, xgd, xgd - xgd_new, 1.0)
+    @. xgd -= gamma * (xgd - xgd_new)
+end
+
+trajectory_arr_fw = Vector{Tuple{Int64,Float64,Float64,Float64,Float64,Float64}}()
+callback = build_callback(trajectory_arr_fw)
+xfin, _, _, _, traj_data = FrankWolfe.frank_wolfe(
+    f,
+    grad!,
+    lmo,
+    x0;
+    epsilon=1e-9,
+    max_iteration=10 * k,
+    print_iter=k / 10,
+    verbose=false,
+    linesearch_tol=1e-8,
+    line_search=FrankWolfe.Adaptive(),
+    emphasis=FrankWolfe.memory,
+    gradient=gradient,
+    callback=callback,
+)
+
+trajectory_arr_lazy = Vector{Tuple{Int64,Float64,Float64,Float64,Float64,Float64}}()
+callback = build_callback(trajectory_arr_lazy)
+xlazy, _, _, _, _ = FrankWolfe.lazified_conditional_gradient(
+    f,
+    grad!,
+    lmo,
+    x0;
+    epsilon=1e-9,
+    max_iteration=10 * k,
+    print_iter=k / 10,
+    verbose=false,
+    linesearch_tol=1e-8,
+    line_search=FrankWolfe.Adaptive(),
+    emphasis=FrankWolfe.memory,
+    gradient=gradient,
+    callback=callback,
+)
+
+
+trajectory_arr_lazy_ref = Vector{Tuple{Int64,Float64,Float64,Float64,Float64,Float64}}()
+callback = build_callback(trajectory_arr_lazy_ref)
+xlazy, _, _, _, _ = FrankWolfe.lazified_conditional_gradient(
+    f,
+    grad!,
+    lmo,
+    x0;
+    epsilon=1e-9,
+    max_iteration=50 * k,
+    print_iter=k / 10,
+    verbose=false,
+    linesearch_tol=1e-8,
+    line_search=FrankWolfe.Adaptive(),
+    emphasis=FrankWolfe.memory,
+    gradient=gradient,
+    callback=callback,
+)
+
+fw_test_values = getindex.(trajectory_arr_fw, 6)
+lazy_test_values = getindex.(trajectory_arr_lazy, 6)
+
+results = Dict("svals_gd"=>svdvals(xgd),
+"svals_fw"=>svdvals(xfin),
+"svals_lcg"=>svdvals(xlazy),
+"fw_test_values"=>fw_test_values,
+"lazy_test_values"=>lazy_test_values,
+"trajectory_arr_fw"=>trajectory_arr_fw,
+"trajectory_arr_lazy"=>trajectory_arr_lazy,
+"function_values_gd"=>function_values,
+"function_values_test_gd"=>function_test_values,
+"timing_values_gd"=>timing_values,
+"trajectory_arr_lazy_ref"=>trajectory_arr_lazy_ref)
+
+ref_optimum = results["trajectory_arr_lazy_ref"][end][2]
+
+iteration_list = [
+    [x[1] + 1 for x in results["trajectory_arr_fw"]],
+    [x[1] + 1 for x in results["trajectory_arr_lazy"]],
+    collect(1:1:length(results["function_values_gd"])),
+]
+time_list = [
+    [x[5] for x in results["trajectory_arr_fw"]],
+    [x[5] for x in results["trajectory_arr_lazy"]],
+    results["timing_values_gd"],
+]
+primal_gap_list = [
+    [x[2] - ref_optimum for x in results["trajectory_arr_fw"]],
+    [x[2] - ref_optimum for x in results["trajectory_arr_lazy"]],
+    [x - ref_optimum for x in results["function_values_gd"]],
+]
+test_list =
+    [results["fw_test_values"], results["lazy_test_values"], results["function_values_test_gd"]]
+
+label = [L"\textrm{FW}", L"\textrm{L-CG}", L"\textrm{GD}"]
+
+FrankWolfe.plot_results(
+    [primal_gap_list, primal_gap_list, test_list, test_list],
+    [iteration_list, time_list, iteration_list, time_list],
+    label,
+    [L"\textrm{Iteration}", L"\textrm{Time}", L"\textrm{Iteration}", L"\textrm{Time}"],
+    [
+        L"\textrm{Primal Gap}",
+        L"\textrm{Primal Gap}",
+        L"\textrm{Test Error}",
+        L"\textrm{Test Error}",
+    ],
+    xscalelog=[:log, :identity, :log, :identity],
+    legend_position=[:bottomleft, nothing, nothing, nothing]
+)
