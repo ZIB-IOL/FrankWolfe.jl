8fcde7657a79e40518a2fe551a0c413d374a52bc
matbesancon <mathieu.besancon@gmail.com>
2025-12-21 19:05:37 +0100

    correct FW import

--- ext/FrankWolfeKrylovKitExt.jl
+++ ext/FrankWolfeKrylovKitExt.jl
@@ -4,14 +4,14 @@ import FrankWolfe
 import KrylovKit
 
 function FrankWolfe.compute_extreme_point(
-    lmo::NuclearNormBallLMO{TL,FrankWolfe.KrylovKitBackend},
+    lmo::FrankWolfe.NuclearNormBallLMO{TL,FrankWolfe.KrylovKitBackend},
     direction::AbstractMatrix{TD};
     kwargs...,
 ) where {TL,TD}
     T = promote_type(TD, TL)
     svd_res = KrylovKit.svdsolve(direction, 1, tol=lmo.backend.tol, maxiter=lmo.backend.maxiter)
     u = -lmo.radius * svd_res[2][1]
-    return RankOneMatrix(u::Vector{T}, svd_res[3][1])
+    return FrankWolfe.RankOneMatrix(u::Vector{T}, svd_res[3][1])
 end
 
 end
