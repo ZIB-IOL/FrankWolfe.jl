9512b4f1233c7749690eda8717f902341eb82d04
matbesancon <mathieu.besancon@gmail.com>
2026-08-31 18:52:32 +0200

    test dim

--- test/utils.jl
+++ test/utils.jl
@@ -41,7 +41,7 @@ end
                     @test M[i, j] ≈ R[i, j]
                 end
             end
-            @test R * FillArrays.Zeros(n) ≈ zeros(n)
+            @test R * FillArrays.Zeros(n) ≈ zeros(2n)
             @testset "Right- left-mul" for _ in 1:5
                 x = rand(rng, n)
                 r1 = R * x
